ead934fb4adc21687abd6aca625fa1ddff6d3920
Jeff Weiss <jweiss@redhat.com>
2010-10-25 18:33:09 -0400

    got xmlrpc client working

--- clojure/src/sm/gui/ui.clj
+++ clojure/src/sm/gui/ui.clj
@@ -2,8 +2,12 @@
   (:import com.redhat.qe.ldtpclient.Element
    [org.apache.xmlrpc.client XmlRpcClient XmlRpcClientConfigImpl]))
 
+(defn javacall "call a method on a java object, given an arbitrary number of args" 
+  [obj name & args]
+  (clojure.lang.Reflector/invokeInstanceMethod obj (str name)
+					       (if args (to-array args) clojure.lang.RT/EMPTY_ARRAY)))
 
-
+;;A map of keywords to the GNOME ui data
 (def windows {:mainWindow  {:id "manage_subscriptions_dialog"
                             :elements {:close-main "button_close"
                                        :add "add_button"
@@ -22,26 +26,28 @@
               :subscribeDialog "dialog_add"})
 
 
-(defn element [name-kw]
+(defn element "Given a keyword, retrieve the window id and element id for it, 
+and return those 2 items in a vector" 
+  [name-kw]
   (let [window (first (filter #(get-in % [:elements name-kw]) (vals windows)))
         elem (get-in window [:elements name-kw])]
     (if elem [(:id window) elem]
       (throw (RuntimeException. (format "%s not found in ui mapping." name-kw))))))
 
 
-(defmacro define-ldtp-call [name args]
-  `(defn ~@(symbol name) ~@(map symbol args)))
-
-; ~@(concat '(.execute *xmlrpc*) ~@(map symbol args))
+(defn define-ldtp-call2 [name args client]
+  (intern *ns* (symbol name) 
+    (fn [& args] 
+      (apply javacall client "execute" name args))))
 
 (defn create-xmlrpc-client [url specfile]
   (let [config (XmlRpcClientConfigImpl.)
         client (XmlRpcClient.)]
-    (. config setServerURL (java.net.URL. url))
-    (. client setConfig config)
-    (def ^{:private true} *xmlrpc* client)
+    (.setServerURL config (java.net.URL. url))
+    (.setConfig client config)
     (let [methods (with-in-str (slurp specfile) (read))]
       (doall 
-        (map (fn [method] (define-ldtp-call (first method) (second method))) 
-             methods)))))
+        (map (fn [method] (define-ldtp-call2 (first method) (second method) client)) 
+             methods)))
+    client))
 
