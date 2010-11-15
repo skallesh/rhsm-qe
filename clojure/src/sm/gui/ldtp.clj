fa5eb66e7a3557d00111081ae11a2fe0802f3594
Jeff Weiss <jweiss@redhat.com>
2010-11-15 09:24:40 -0500

    formatted code

--- clojure/src/sm/gui/ldtp.clj
+++ clojure/src/sm/gui/ldtp.clj
@@ -1,16 +1,18 @@
 (ns sm.gui.ldtp
   (:use 'clojure.contrib.pprint)
   (:import java.util.NoSuchElementException
-    [org.apache.xmlrpc.client XmlRpcClient XmlRpcClientConfigImpl]))
+	   [org.apache.xmlrpc.client XmlRpcClient XmlRpcClientConfigImpl]))
 
 (defn element-getter "Returns a function that, given a keyword, retrieves 
 the window id and element id from the given element map, and return those 2 items in a vector" 
   [elem-map]
   (fn [name-kw] 
-    (let [window (first (filter #(get-in % [:elements name-kw]) (vals elem-map)))
-        elem (get-in window [:elements name-kw])]
-    (if elem [(:id window) elem]
-      (throw (NoSuchElementException. (format "%s not found in ui mapping." name-kw)))))))
+    (let [window (first (filter #(get-in % [:elements name-kw])
+				(vals elem-map)))
+	  elem (get-in window [:elements name-kw])]
+      (if elem [(:id window) elem]
+	  (throw (NoSuchElementException.
+		  (format "%s not found in ui mapping." name-kw)))))))
 
 (def client 
   (let [config (XmlRpcClientConfigImpl.)
@@ -22,18 +24,19 @@ the window id and element id from the given element map, and return those 2 item
 (defmacro defxmlrpc
     [specfile]
     (let [methods (with-in-str (slurp specfile) (read))
-          defs (map (fn [[fnname args]]
-                      (let [argsyms (map symbol args)]
-                        `(defn ~(symbol fnname) ~(vec argsyms)
-                           (clojure.lang.Reflector/invokeInstanceMethod
-                              client "execute" (to-array (list ~fnname ~(concat '(list) argsyms)))))))
-                 methods)]
+          defs (map
+		(fn [[fnname args]]
+		  (let [argsyms (map symbol args)]
+		    `(defn ~(symbol fnname) ~(vec argsyms)
+		       (clojure.lang.Reflector/invokeInstanceMethod
+			client "execute" (to-array (list ~fnname ~(concat '(list) argsyms)))))))
+		methods)]
       `(do
          ~@defs)))
 
 (defn set-url [url]
-   (.setConfig client 
-     (.setServerURL (.getConfig client) (java.net.URL. url))))
+  (.setConfig client 
+	      (.setServerURL (.getConfig client) (java.net.URL. url))))
 
 (defxmlrpc "clojure/src/sm/gui/ldtp_api.txt")
 
