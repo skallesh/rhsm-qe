5295bb219585f25a8f99282ac23099fb650bb505
Jeff Weiss <jweiss@redhat.com>
2010-12-17 10:49:23 -0500

    finalizing error handling - now normal java exceptions can participate

--- clojure/src/sm/gui/errors.clj
+++ clojure/src/sm/gui/errors.clj
@@ -45,10 +45,6 @@
 
 
 (def *handlers* [])
-(defn- wrap [m]
-  (let [e (NamingException. (or (:msg m) ""))]
-    (.setResolvedObj e m)
-    e))
 
 (defn- rewrap [e addmap]
   (let [m (unwrap e)
@@ -56,16 +52,34 @@
     (.setResolvedObj e m)
     e))
 
-(defn- unwrap [e]
-  (let [r (.getResolvedObj e)]
-    (if (map? r) r (throw (IllegalStateException. "Wrapped object is not a map - must be a real NamingException?")))))
+(defn- e-to-map [e]
+  {:msg (.getMessage e) :type (class e) :exception e})
 
+(defn- unwrap [e]
+  (if (instance? NamingException e) (let [r (.getResolvedObj e)]
+                                      (if (map? r) r
+                                          (throw (IllegalStateException.
+                                                  "Wrapped object is not a map - must be a real NamingException?"))))
+      (e-to-map e)))
+
+(defn- is-type [m ptype]
+  (isa? (:type this) ptype))
+  
 (defprotocol Raisable
-  (raise [this]))
+  (raise [this])
+  (wrap [this]))
 
 (extend-protocol Raisable
   clojure.lang.IPersistentMap
-  (raise [this] (throw (wrap this))))
+  (raise [this] (throw (wrap this)))
+  (wrap [this] (let [e (NamingException. (or (:msg this) ""))]
+               (.setResolvedObj e this)
+               e)))
+
+  java.lang.Throwable
+  (raise [this] (raise (e-to-map this)))
+  (wrap [this] (wrap (e-to-map this))))
+
 
 (defn- equal-or-more? [m1 m2]
   (cond (= m1 m2) true
@@ -73,18 +87,17 @@
         (= m1 (select-keys m2 (keys m1))) true))
 
 
-(defn recover [recovery err]
+(defn- recover [recovery err]
   (let [recoveryfn (recovery err)]
     (cond (nil? recoveryfn) (throw (IllegalStateException. (str "Recovery chosen that does not exist: " recovery)))
           (fn? recoveryfn) (recoveryfn err)
-          :else (throw (IllegalArgumentException. (format "Recovery %s needs to a function with one argument, instead got: %s" recovery recoveryfn))))))
+          :else (throw (IllegalArgumentException. (format "Recovery %s needs to be a function with one argument, instead got: %s" recovery recoveryfn))))))
 
 (defn- dispatch "Thread the map m through all the handler functions
 in hlist, until one of them returns something other than m.
 Return the first non-m value, or m if the end of
 the list is hit."
   [m hlist]
-  (println hlist)
   (let [handled (drop-while  #(= m %)
                              (reductions #(%2 %1) m hlist))]
     (if (> (count handled) 0)
@@ -104,33 +117,40 @@ an error as an argument, and returns one of the following:
   3) Finally the handler can choose a pre-defined recovery by
   returning a map with a single entry like {:recovery :abort} where
   the value is the recovery name to invoke."  [hlist & body]
-  (if-not (and (coll? hlist) (every? coll? hlist)) (throw (IllegalArgumentException. "First argument to with-handler must be a collection of handlers")))
+  (if-not (and (coll? hlist) (every? coll? hlist))
+    (throw (IllegalArgumentException. "First argument to with-handler must be a collection of handlers")))
   `(binding [*handlers* (concat ~hlist *handlers*) ] ;chain handlers together
      (try ~@body
-          (catch NamingException ne#
+          (catch Throwable ne#
             (let [unwrapped# (unwrap ne#)
-                  handler-result# (dispatch unwrapped# *handlers*)]
-              (cond (equal-or-more? unwrapped# handler-result#) (throw ne#) ;returning the original map means unhandled
+                  handler-result# (dispatch unwrapped# *handlers*)
+                  unhandled# (or (:exception unwrapped#) ne#)]  ;if the original error was an exception, retrieve it to throw if it is not handled.
+              (cond (equal-or-more? unwrapped# handler-result#) (throw unhandled#) ;returning the original map means unhandled
                     (and (map? handler-result#) (:recovery handler-result#))
                     (recover (:recovery handler-result#) unwrapped#)
                     :else handler-result#))))))
 
 (defmacro add-recoveries [m & body]
   `(try ~@body
-        (catch NamingException ne#
+        (catch Throwable ne#
           (throw (rewrap ne# ~m)))))
 
 (defmacro handle-type "A convenience macro that creates an error
-handler by error type. It will match any error whose :type entry is
-type-kw. The rest of the error map entries are ignored.  If you need
+handler by error type. It will match any error e where (isa? (:type e)
+type). The rest of the error map entries are ignored.  If you need
 more access to the error map, you shouldn't use this."
-  [type-kw & body]
-  `(fn [e#] (if (= (:type e#) ~type-kw)
+  [type & body]
+  `(fn [e#] (if (isa? (:type e#) ~type)
             (do ~@body)
             e#)))
 
 (defn error-prone [n]
-  (if (> n 0) (inc n) (raise {:msg "Negative number!" :number n :type :NumberError})))
+  (cond 
+        (> n 200) (raise (IllegalStateException. "Wayy Too big!"))
+        (> n 100) (throw (IllegalArgumentException. "Too big!"))
+        (> n 0) (inc n)
+        :else (raise {:msg "Negative number!" :number n :type :NumberError})))
+
 
 (comment (defn do-stuff [n]
            (add-recoveries {:zero (fn [e] 0)
@@ -139,8 +159,10 @@ more access to the error map, you shouldn't use this."
 
          (with-handlers (fn [e] (if (= (:type e) :NumberError) {:recovery :retry} )) (do-stuff -5))
          (with-handlers
-           [ (handle-type :NumberErrorBlah {:recovery :one})
-             (handle-type :OtherError 0)]
-           (add-recoveries {:one (fn [e] 1)} (do-stuff -5)))
+           [ (handle-type :NumberError {:recovery :one})
+             (handle-type :OtherError 0)
+             (handle-type IllegalStateException 201)]
+           (add-recoveries {:one (fn [e] 1)} (do-stuff 205)))
+         
          )
 
