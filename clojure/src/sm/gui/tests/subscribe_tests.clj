96983384a8607140cc7588359318986a0bf5f91e
Jeff Weiss <jweiss@redhat.com>
2010-12-20 14:17:51 -0500

    added subscribe tests

---
+++ clojure/src/sm/gui/tests/subscribe_tests.clj
@@ -0,0 +1,16 @@
+(ns sm.gui.tests.subscribe-tests
+  (:use [test-clj.testng :only (gen-class-testng)]
+	[sm.gui.test-config :only (config)]
+        [com.redhat.qe.handler :only (handle-type with-handlers recover-by)]
+	 sm.gui.ldtp)
+  (:require [sm.gui.tasks :as tasks])
+  (:import [org.testng.annotations Test]))
+
+(defn ^{Test {:groups ["subscribe"]}} subscribe_all [_]
+  (let [all-available (for [row (range (- (action getrowcount :all-subscriptions-view) 1))]
+                         (action getcellvalue :all-subscriptions-view row 0))]
+    (for [subscription all-available]
+      (with-handlers [(handle-type :wrong-consumer-type [e] (recover-by :log-warning))]
+        (tasks/subscribe subscription)))))
+
+(gen-class-testng)
