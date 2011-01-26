801c1c32ee38e0a7f4c55bcc39c56ab3fc8a2388
jmolet <jmolet@x2112.usersys.redhat.com>
2011-01-26 10:19:38 -0500

    added proxy automation to ui testing

--- clojure/src/sm/gui/tests/proxy_tests.clj
+++ clojure/src/sm/gui/tests/proxy_tests.clj
@@ -1,8 +1,9 @@
 (ns sm.gui.tests.proxy-tests
   (:use [test-clj.testng :only (gen-class-testng)]
-	[sm.gui.test-config :only (config)]
+	[sm.gui.test-config :only (config clientcmd)]
         [com.redhat.qe.verify :only (verify)]
         [error.handler :only (with-handlers handle ignore recover)]
+        [clojure.contrib.str-utils :only (re-split)]
 	 sm.gui.ldtp)
   (:require [sm.gui.tasks :as tasks])
   (:import [org.testng.annotations Test BeforeClass]))
@@ -12,8 +13,25 @@
   (with-handlers [(ignore :not-registered)]
     (tasks/unregister)))
 
+(defn conf-file-value [k]
+  (->> (.getStdout (.runCommandAndWait @clientcmd (str "grep " k " /etc/rhsm/rhsm.conf"))) (re-split #"=") last .trim))
+
 (defn ^{Test {:groups ["proxy"]}}
   enable_proxy [_]
-  )
+  (let [hostname (@config :basicauth-proxy-hostname)
+        port (@config :basicauth-proxy-port)
+        username (@config :basicauth-proxy-port)
+        password (@config :basicauth-proxy-password)]
+    (tasks/enableproxy hostname port username password)
+    (println "finished ui")
+    (let [config-file-hostname (conf-file-value "proxy_hostname")
+          config-file-port (conf-file-value "proxy_port")
+          config-file-user (conf-file-value "proxy_user")
+          config-file-password (conf-file-value "proxy_password")]
+      (println "finished ssh")
+      (verify (= config-file-hostname hostname))
+      (verify (= config-file-port port))
+      (verify (= config-file-user (username)))
+      (verivy (= config-file-password password)) )))
 
 
