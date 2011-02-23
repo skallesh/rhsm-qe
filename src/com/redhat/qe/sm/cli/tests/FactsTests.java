0bb99284fddf7bf5a0d0c07821534c857a838dad
John Sefler <jsefler@redhat.com>
2011-02-23 14:31:31 -0500

    added TODO 629670

--- src/com/redhat/qe/sm/cli/tests/FactsTests.java
+++ src/com/redhat/qe/sm/cli/tests/FactsTests.java
@@ -329,6 +329,7 @@ public class FactsTests extends SubscriptionManagerCLITestScript{
 	// TODO Candidates for an automated Test:
 	//		https://bugzilla.redhat.com/show_bug.cgi?id=669513
 	//		https://bugzilla.redhat.com/show_bug.cgi?id=664847#2
+	//		https://bugzilla.redhat.com/show_bug.cgi?id=629670
 
 	
 	
