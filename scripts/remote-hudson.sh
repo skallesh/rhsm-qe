f9a45a206bf2d1e9098eb0919b74bc472cefe1d8
jmolet <jmolet@redhat.com>
2011-03-16 14:28:40 -0400

    added beakerCheckout and remote-hudson to sm scripts

---
+++ scripts/remote-hudson.sh
@@ -0,0 +1,40 @@
+#!/bin/bash
+
+URL="http://hudson.rhq.lab.eng.bos.redhat.com:8080/hudson/view/Entitlement/job/rhsm-beaker-on-premises/build"
+
+#rhel6-x86_64
+JSON="{\"parameter\": [{\"name\": \"CLIENT1_ARCH\",         \"value\": \"x86_64\"} , \
+                       {\"name\": \"CLIENT1_DistroFamily\", \"value\": \"RedHatEnterpriseLinux6\"}], \"\": \"\"}"
+                       
+echo "Queing Job:  curl -X POST $URL -d token=hudsonbeaker-remote --data-urlencode json='$JSON'"
+curl -X POST $URL -d token=hudsonbeaker-remote --data-urlencode json="$JSON"
+sleep 10
+
+#rhel6-i386
+JSON="{\"parameter\": [{\"name\": \"CLIENT1_ARCH\",         \"value\": \"i386\"} , \
+                       {\"name\": \"CLIENT1_DistroFamily\", \"value\": \"RedHatEnterpriseLinux6\"}], \"\": \"\"}"
+echo "Queing Job:  curl -X POST $URL -d token=hudsonbeaker-remote --data-urlencode json='$JSON'"
+curl -X POST $URL -d token=hudsonbeaker-remote --data-urlencode json="$JSON"
+sleep 10
+
+#rhel6-ia64
+JSON="{\"parameter\": [{\"name\": \"CLIENT1_ARCH\",         \"value\": \"ia64\"} , \
+                       {\"name\": \"CLIENT1_DistroFamily\", \"value\": \"RedHatEnterpriseLinux6\"}], \"\": \"\"}"
+echo "Queing Job:  curl -X POST $URL -d token=hudsonbeaker-remote --data-urlencode json='$JSON'"
+curl -X POST $URL -d token=hudsonbeaker-remote --data-urlencode json="$JSON"
+sleep 10
+
+#rhel6-ppc64
+JSON="{\"parameter\": [{\"name\": \"CLIENT1_ARCH\",         \"value\": \"ppc64\"} , \
+                       {\"name\": \"CLIENT1_DistroFamily\", \"value\": \"RedHatEnterpriseLinux6\"}], \"\": \"\"}"
+echo "Queing Job:  curl -X POST $URL -d token=hudsonbeaker-remote --data-urlencode json='$JSON'"
+curl -X POST $URL -d token=hudsonbeaker-remote --data-urlencode json="$JSON"
+sleep 10
+
+#rhel6-s390x
+JSON="{\"parameter\": [{\"name\": \"CLIENT1_ARCH\",         \"value\": \"x390x\"} , \
+                       {\"name\": \"CLIENT1_DistroFamily\", \"value\": \"RedHatEnterpriseLinux6\"}], \"\": \"\"}"
+echo "Queing Job:  curl -X POST $URL -d token=hudsonbeaker-remote --data-urlencode json='$JSON'"
+curl -X POST $URL -d token=hudsonbeaker-remote --data-urlencode json="$JSON"
+sleep 10
+
