84deef1ce39c885456ded8da0b3b2db0fa7f2007
jmolet <jmolet@redhat.com>
2011-06-03 16:13:02 -0400

    adding get_latest_rpm.py

---
+++ scripts/get_latest_rpm.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python
+# mod_python wrapper around latest_rpm.py
+
+from latest_rpm import *
+from mod_python import apache, util
+
+BASE_URL = "http://download.devel.redhat.com/brewroot/packages/%s/"
+
+def handler(request):
+    request.headers_out.add("version", "0.1")
+    request.send_http_header()
+    
+    params = util.FieldStorage(request)
+
+    baseurl = params.get('baseurl', BASE_URL)
+    rpmname = params.get('rpmname','')
+    basegrp = params.get('basegrp',rpmname)
+    getlink = params.get('getlink', 'false')
+    arch    = params.get('arch', "")
+    version = params.get('version','')
+
+    rpmurl = find_latest_rpm_url(baseurl%basegrp, arch, rpmname, version)
+    if 'true' in getlink:
+        request.content_type = 'text/plain'
+        request.send_http_header()
+        request.write(rpmurl)
+    else:
+        rpm = urllib2.urlopen(rpmurl)
+        request.content_type = 'application/x-rpm'
+        request.send_http_header()
+        request.write(rpm.read())
+    return apache.OK
---
+++ scripts/latest_rpm.py
@@ -0,0 +1,101 @@
+#!/usr/lib/python
+# Latest Brew RPM Finder
+# Digs through Brewroot directories to find the latest release of an RPM
+# place in /var/www/python/
+# Author: Steve Salevan <ssalevan@redhat.com>
+# Author: James Molet <jmolet@redhat.com> (current)
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU Library General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+
+import urllib2
+import re
+import sys
+import pdb
+
+from itertools import groupby
+from BeautifulSoup import BeautifulSoup
+
+VERSION_REGEX = "[0-9]+\.[0-9]+/"
+EPOCH_REGEX   = "\d+\.\w+/"
+
+def get_links_matching_regex(regex, page):
+    soup = BeautifulSoup(page)
+    links = soup.findAll('a')
+    matches = []
+    for link in links:
+        if (re.search(regex, link.string)):
+            matches.append(link)
+    return matches
+
+def sort_by_version(links):
+    versions = []
+    for item in links: versions.append(str(item['href'].replace('/','')))
+    
+    demarcations=0
+    for item in versions:
+        fields = len(item.split("."))
+        if fields > demarcations:
+            demarcations = fields
+
+    version_list = []
+    for item in versions:
+        fields = len(item.split("."))
+        version_list.append(item + ('.0')*(demarcations-fields))
+
+    cur_list = version_list
+
+    n=1
+    while n < demarcations:
+        new_list = []
+        group_func = lambda x: ".".join(x.split(".")[0:-n])
+        sort_key_func = lambda x: int(x.split(".")[demarcations - n])
+
+        cur_list.sort(key=group_func)
+
+        for key, group in groupby(cur_list, group_func):
+            group_list = list(group)
+            group_list.sort(key=sort_key_func)
+            new_list += group_list
+
+        cur_list = new_list
+        n += 1
+   
+    for item in cur_list:
+        cur_list[cur_list.index(item)] = str(item.rstrip('.0') + '/')
+
+    return cur_list
+ 
+
+def find_latest_rpm_url(baseurl, arch, rpm_name, version=''):
+    version_page = urllib2.urlopen(baseurl)
+    if version == '':
+        REGEX = VERSION_REGEX
+    else:
+        REGEX = version + "[\.\d+]*/"
+    links = get_links_matching_regex(REGEX,version_page)
+    links = sort_by_version(links)
+    version_append = links[len(links)-1]
+    
+    epoch_page = urllib2.urlopen(baseurl+version_append)
+    links = get_links_matching_regex(EPOCH_REGEX,epoch_page)
+    epoch_append = links[len(links)-1]['href']
+    rpm_page = urllib2.urlopen(baseurl+version_append+epoch_append+arch+'/')
+    links = get_links_matching_regex(rpm_name, rpm_page)
+    rpm_append = links[0]['href']
+    return baseurl+version_append+epoch_append+arch+'/'+rpm_append
+
+if __name__ in "__main__":
+    print find_latest_rpm_url(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
+
