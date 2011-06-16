b6107c5ae834971a13c39543944dd137f9ea13dc
jmolet <jmolet@redhat.com>
2011-06-16 16:31:22 -0400

    adding data provided tests

--- src/com/redhat/qe/sm/gui/tests/subscribe_tests.clj
+++ src/com/redhat/qe/sm/gui/tests/subscribe_tests.clj
@@ -28,45 +28,57 @@
   (tasks/search {})
   (to-array-2d (map vector (tasks/get-table-elements :all-subscriptions-view 0))))
 
-(comment 
-  (defn ^{Test {:groups ["subscribe"]}}
-    subscribe_all [_]
-    (tasks/search {})
-    (tasks/do-to-all-rows-in :all-subscriptions-view 0
-                             (fn [subscription]
-                               (with-handlers [(ignore :subscription-not-available)
-                                               (handle :wrong-consumer-type [e]
-                                                       (recover e :log-warning))]
-                                 (tasks/subscribe subscription)))))
 
-  (defn ^{Test {:groups ["subscribe"]
-                :dependsOnMethods [ "subscribe_all"]}}
-    unsubscribe_all [_]
-    (tasks/ui selecttab :my-subscriptions)
-    (tasks/do-to-all-rows-in :my-subscriptions-view 0
-                             (fn [subscription] (with-handlers [(ignore :not-subscribed)]
-                                                 (tasks/unsubscribe subscription)
-                                                 (verify (= (tasks/ui rowexist? :my-subscriptions-view subscription) false)))))))
+(defn ^{Test {:groups ["subscribe"]
+              :dataProvider "subscribed"}}
+  unsubscribe_each [_ subscription]
+  (tasks/ui selecttab :my-subscriptions)
+  (with-handlers [(ignore :not-subscribed)]
+    (tasks/unsubscribe subscription)
+    (verify (= false (tasks/ui rowexist? :my-subscriptions-view subscription)))))
+
+(defn ^{DataProvider {:name "subscribed"}}
+  get_subscribed [_]
+  (tasks/ui selecttab :my-subscriptions)
+  (to-array-2d (map vector (tasks/get-table-elements :my-subscriptions-view 0))))
+
+(defn
+  subscribe_all [_]
+  (tasks/search {})
+  (tasks/do-to-all-rows-in :all-subscriptions-view 0
+                           (fn [subscription]
+                                (with-handlers [(ignore :subscription-not-available)
+                                                (handle :wrong-consumer-type [e]
+                                                        (recover e :log-warning))]
+                                  (tasks/subscribe subscription)))))
+
+(defn
+  unsubscribe_all [_]
+  (tasks/ui selecttab :my-subscriptions)
+  (tasks/do-to-all-rows-in :my-subscriptions-view 0
+                           (fn [subscription] (with-handlers [(ignore :not-subscribed)]
+                                               (tasks/unsubscribe subscription)
+                                               (verify (= (tasks/ui rowexist? :my-subscriptions-view subscription) false))))))
 
 
 
 (comment
-;; https://bugzilla.redhat.com/show_bug.cgi?id=679961                                         
-(defn ^{Test {:groups ["subscribe"]}}
-  check_unsubscribe_clear [_]
-  (tasks/search {})
-  (let [subscripton (tasks/ui getcellvalue :all-subscriptions-view 0 0)]
-    ;;add error handler?
-    (tasks/subscribe subscritpion))
-  (tasks/ui selecttab :my-subscriptions)
-  (let [subscripton (tasks/ui getcellvalue :my-subscriptions-view 0 0)]
-    ;;verify that info field populated
-    (tasks/unsubscribe subscription)
-    ;;verify that info field cleared
+  ;; https://bugzilla.redhat.com/show_bug.cgi?id=679961                                         
+  (defn ^{Test {:groups ["subscribe"]}}
+    check_unsubscribe_clear [_]
+    (tasks/search {})
+    (let [subscripton (tasks/ui getcellvalue :all-subscriptions-view 0 0)]
+      ;;add error handler?
+      (tasks/subscribe subscritpion))
+    (tasks/ui selecttab :my-subscriptions)
+    (let [subscripton (tasks/ui getcellvalue :my-subscriptions-view 0 0)]
+      ;;verify that info field populated
+      (tasks/unsubscribe subscription)
+      ;;verify that info field cleared
+      )
     )
-)
 
-)
+  )
 ;; TODO https://bugzilla.redhat.com/show_bug.cgi?id=683550
 ;; TODO https://bugzilla.redhat.com/show_bug.cgi?id=691784
 ;; TODO https://bugzilla.redhat.com/show_bug.cgi?id=691788
