2f8730bd832d15c098fdc0482b538db22f1da3f8
jmolet <jmolet@redhat.com>
2011-07-14 11:20:34 -0400

    fixing facts-tests

--- src/com/redhat/qe/sm/gui/tests/facts_tests.clj
+++ src/com/redhat/qe/sm/gui/tests/facts_tests.clj
@@ -9,6 +9,18 @@
             com.redhat.qe.sm.gui.tasks.ui)
   (:import [org.testng.annotations BeforeClass BeforeGroups Test DataProvider]))
 
+(def gui-facts (atom nil))
+(def cli-facts (atom nil))
+
+(defn get-cli-facts []
+  (let [allfacts (.getStdout
+                  (.runCommandAndWait @clientcmd
+                                      "subscription-manager facts --list"))
+        allfactpairs (split-lines allfacts)
+        factslist (into {} (map (fn [fact] (vec (split #": " fact)))
+                                allfactpairs))]
+    factslist))
+
 (defn ^{BeforeClass {:groups ["facts"]}}
   register [_]
   (let [ownername (tasks/get-owner-display-name (@config :username)
@@ -18,33 +30,22 @@
                             (recover e :unregister-first))]
       (tasks/register (@config :username)
                       (@config :password)
-                      :owner ownername))))
-
-
-(def cli_facts
-  (let [allfacts (.getStdout
-                  (.runCommandAndWait @clientcmd
-                                      "subscription-manager facts --list"))
-        allfactpairs (split-lines allfacts)
-        factslist (into {} (map (fn [fact] (vec (split #": " fact)))
-                                allfactpairs))]
-     factslist))
-
-(def gui_facts (tasks/get-all-facts))
-
+                      :owner ownername)))
+  (reset! gui-facts (tasks/get-all-facts))
+  (reset! cli-facts (get-cli-facts))) 
 
 (defn ^{Test {:groups ["facts"]
               :dataProvider "guifacts"}}
   match_each_fact [_ fact value]
-  (verify (= (cli_facts fact) value)))
+  (verify (= (cli-facts fact) value)))
 
 (defn ^{Test {:groups ["facts"]}}
   facts_parity [_]
-  (verify (= (count cli_facts)
-             (count gui_facts))))
+  (verify (= (count @cli-facts)
+             (count @gui-facts))))
 
 
 (defn ^{DataProvider {:name "guifacts"}}
   get_facts [_]
-  (to-array-2d (vec (gui_facts))))
+  (to-array-2d (vec @gui-facts)))
 
