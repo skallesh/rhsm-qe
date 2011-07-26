a638f5cedbb2f63a02b7fb48c4adc288aacb937f
jmolet <jmolet@redhat.com>
2011-07-26 10:04:08 -0400

    typo

--- src/com/redhat/qe/sm/gui/tests/register_tests.clj
+++ src/com/redhat/qe/sm/gui/tests/register_tests.clj
@@ -55,16 +55,16 @@
 (defn ^{DataProvider {:name "userowners"}}
   get_userowners [_]
   (to-array-2d
-   (into (if (and (@config :username1) (@config :password1))
+   (conj (if (and (@config :username1) (@config :password1))
            (get-userlists (@config :username1) (@config :password1)))
          (if (and (@config :username) (@config :password))
            (get-userlists (@config :username) (@config :password)))
          ; https://bugzilla.redhat.com/show_bug.cgi?id=719378
          (if (and (@config :username) (@config :password))
-           (vector [(str (@config :username) "   ") (@config :password) nil]))
+           [(str (@config :username) "   ") (@config :password) nil])
          ; https://bugzilla.redhat.com/show_bug.cgi?id=719378
          (if (and (@config :username) (@config :password))
-           (vector [(str "   " (@config :username)) (@config :password) nil])))))
+           [(str "   " (@config :username)) (@config :password) nil]))))
 
 
 (data-driven register_bad_credentials {Test {:groups ["registration"]}}
