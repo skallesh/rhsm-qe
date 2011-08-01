00433ecb82c5799147a6508da2995478bb8b829e
John Sefler <jsefler@redhat.com>
2011-08-01 18:12:09 -0400

    added TODO Bug 687970

--- src/com/redhat/qe/sm/cli/tests/ContentTests.java
+++ src/com/redhat/qe/sm/cli/tests/ContentTests.java
@@ -288,7 +288,11 @@ public class ContentTests extends SubscriptionManagerCLITestScript{
 	// TODO Bug 689031 - nss needs to be able to use pem files interchangeably in a single process 
 	// TODO Bug 701425 - NSS issues with more than one susbcription 
 	// TODO Bug 706265 - product cert is not getting removed after removing all the installed packages from its repo using yum
-	
+	// TODO Bug 687970 - Currently no way to delete a content source from a product
+	// how to create content (see Bug 687970): [jsefler@jsefler ~]$ curl -u admin:admin -k --request POST https://jsefler-onprem-62candlepin.usersys.redhat.com:8443/candlepin/content --header "Content-type: application/json" -d '{"contentUrl":"/foo/path","label":"foolabel","type":"yum","gpgUrl":"/foo/path/gpg","id":"fooid","name":"fooname","vendor":"Foo Vendor"}' | python -m json.tool
+	// how to delete content (see Bug 687970): [jsefler@jsefler ~]$ curl -u admin:admin -k --request DELETE https://jsefler-onprem-62candlepin.usersys.redhat.com:8443/candlepin/content/fooid
+	// how to get content    (see Bug 687970): [jsefler@jsefler ~]$ curl -u admin:admin -k --request GET https://jsefler-onprem-62candlepin.usersys.redhat.com:8443/candlepin/content/fooid
+	// how to associate content with product   (see Bug 687970): [jsefler@jsefler ~]$ curl -u admin:admin -k --request POST https://jsefler-onprem-62candlepin.usersys.redhat.com:8443/candlepin/product/productid/content/fooid&enabled=false
 
 	
 	// Configuration Methods ***********************************************************************
