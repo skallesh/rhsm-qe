6be3dd90e988af853b154622c8ad76ddc8edd4fe
jmolet <jmolet@redhat.com>
2011-08-26 11:11:49 -0400

    creating acceptance tests

---
+++ src/com/redhat/qe/sm/gui/tests/acceptance_tests.clj
@@ -0,0 +1,39 @@
+(ns com.redhat.qe.sm.gui.tests.acceptance-tests
+  (:use [test-clj.testng :only [gen-class-testng data-driven]]
+        [com.redhat.qe.sm.gui.tasks.test-config :only (config)]
+        [com.redhat.qe.verify :only (verify)]
+        [error.handler :only (with-handlers handle ignore recover)]
+        gnome.ldtp)
+  (:require [com.redhat.qe.sm.gui.tasks.tasks :as tasks]
+            [com.redhat.qe.sm.gui.tests.register-tests :as retst]
+            [com.redhat.qe.sm.gui.tests.subscribe-tests :as setst]
+            [com.redhat.qe.sm.gui.tests.subscription-assistant-tests :as saetst])
+  (:import [org.testng.annotations Test BeforeClass DataProvider]))
+
+
+(defn ^{Test {:groups ["acceptance"]}}
+  register [_]
+  (rtest/simple_register nil
+                         (@config :username)
+                         (@config :password)
+                         nil))
+
+(defn ^{Test {:groups ["acceptance"]}}
+  unregister [_]
+  (rtest/unregister nil))
+
+(defn- check-register []
+  (if (tasks/ui exists? :register-system)
+    (register nil)))
+
+(defn ^{Test {:groups ["acceptance"]}}
+  subscribe-all [_]
+  (check-register)
+  (stest/subscribe_all))
+
+(defn ^{Test {:groups ["acceptance"]}}
+  unsubscribe-all [_]
+  (check-register)
+  (stest/unsubscribe_all))
+
+(gen-class-testng)
