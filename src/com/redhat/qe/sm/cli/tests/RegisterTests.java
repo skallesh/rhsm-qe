69c8d23fcb7ebe7009c43c8ded1ba5a5677e7b3d
jsefler <jsefler@jseflerT510.usersys.redhat.com>
2011-08-28 23:02:26 -0400

    translation team fixed es_ES message Credenciales inválidos

--- src/com/redhat/qe/sm/cli/tests/RegisterTests.java
+++ src/com/redhat/qe/sm/cli/tests/RegisterTests.java
@@ -862,7 +862,7 @@ Expected Results:
 		if (sm_isServerOnPremises) {
 			ll.add(Arrays.asList(new Object[]{null,								"en_US.UTF8", sm_clientUsername+getRandInt(), sm_clientPassword+getRandInt(), 255, null, "Invalid Credentials"}));
 			ll.add(Arrays.asList(new Object[]{new BlockedByBzBug("615362"),		"de_DE.UTF8", sm_clientUsername+getRandInt(), sm_clientPassword+getRandInt(), 255, null, "Ungültige Berechtigungnachweise"}));
-			ll.add(Arrays.asList(new Object[]{null,								"es_ES.UTF8", sm_clientUsername+getRandInt(), sm_clientPassword+getRandInt(), 255, null, "Credenciales inválidas"}));
+			ll.add(Arrays.asList(new Object[]{null,								"es_ES.UTF8", sm_clientUsername+getRandInt(), sm_clientPassword+getRandInt(), 255, null, "Credenciales inválidos"}));
 			ll.add(Arrays.asList(new Object[]{null,								"fr_FR.UTF8", sm_clientUsername+getRandInt(), sm_clientPassword+getRandInt(), 255, null, "Informations d’identification invalides"}));
 			ll.add(Arrays.asList(new Object[]{null,								"it_IT.UTF8", sm_clientUsername+getRandInt(), sm_clientPassword+getRandInt(), 255, null, "Credenziali invalide"}));
 			ll.add(Arrays.asList(new Object[]{null,								"ja_JP.UTF8", sm_clientUsername+getRandInt(), sm_clientPassword+getRandInt(), 255, null, "無効な識別情報"}));
