c93773f04fd8ca4f0639e4e214c9418d6bbde292
John Sefler <jsefler@redhat.com>
2011-09-22 16:06:34 -0400

    added TODO Bug 706853 - SM Gui “unregister” button deletes “consumer” folder for non network host.

--- src/com/redhat/qe/sm/cli/tests/UnregisterTests.java
+++ src/com/redhat/qe/sm/cli/tests/UnregisterTests.java
@@ -40,4 +40,5 @@ public class UnregisterTests extends SubscriptionManagerCLITestScript {
 	
 	// Candidates for an automated Test:
 	// TODO Bug 674652 - Subscription Manager Leaves Broken Yum Repos After Unregister
+	// TODO Bug 706853 - SM Gui “unregister” button deletes “consumer” folder for non network host.
 }
