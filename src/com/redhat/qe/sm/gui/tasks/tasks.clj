e5f3dcb661258251f412ae254e0243932b357099
jmolet <jmolet@redhat.com>
2011-10-02 14:09:20 -0400

    adding changes for jsefler

--- src/com/redhat/qe/sm/gui/tasks/tasks.clj
+++ src/com/redhat/qe/sm/gui/tasks/tasks.clj
@@ -11,7 +11,8 @@
   (:require [clojure.contrib.logging :as log]
             com.redhat.qe.sm.gui.tasks.ui) ;;need to load ui even if we don't refer to it because of the extend-protocol in there.
   (:import [com.redhat.qe.tools RemoteFileTasks]
-           [com.redhat.qe.sm.cli.tasks CandlepinTasks]))
+           [com.redhat.qe.sm.cli.tasks CandlepinTasks]
+           [com.redhat.qe.sm.base SubscriptionManagerBaseTestScript]))
 
 
 (def ui gnome.ldtp/action) ;;alias action in ldtp to ui here
@@ -460,54 +461,38 @@
   "Given a username and password, this function returns a list
   of owners associated with that user"
   [username password]
-  (let [server (conf-file-value "hostname")
-        port (conf-file-value "port")
-        prefix (conf-file-value "prefix")]
-    (seq (CandlepinTasks/getOrgsKeyValueForUser server
-                                                port
-                                                prefix
-                                                username
+  (let [url (SubscriptionManagerBaseTestScript/sm_serverUrl)]
+    (seq (CandlepinTasks/getOrgsKeyValueForUser username
                                                 password
+                                                url
                                                 "displayName"))))
 
 (defn get-owner-display-name
   "Given a owner key (org key) this returns the owner's display name"
   [username password orgkey]
-  (let [server (conf-file-value "hostname")
-        port (conf-file-value "port")
-        prefix (conf-file-value "prefix")]
-    (CandlepinTasks/getOrgDisplayNameForOrgKey server
-                                               port
-                                               prefix
-                                               username
-                                               password
-                                               orgkey)))
+  (let [url (SubscriptionManagerBaseTestScript/sm_serverUrl)]
+    (CandlepinTasks/getOrgDisplayNameForOrgKey  username
+                                                password
+                                                url
+                                                orgkey)))
 
 (defn get-pool-id
   "Get the pool ID for a given subscription/contract pair."
   [username password orgkey subscription contract]
-  (let [server (conf-file-value "hostname")
-        port (conf-file-value "port")
-        prefix (conf-file-value "prefix")]
-    (CandlepinTasks/getPoolIdFromProductNameAndContractNumber server
-                                                              port
-                                                              prefix
-                                                              username
+  (let [url (SubscriptionManagerBaseTestScript/sm_serverUrl)]
+    (CandlepinTasks/getPoolIdFromProductNameAndContractNumber username
                                                               password
+                                                              url
                                                               orgkey
                                                               subscription
                                                               contract)))
 (defn multi-entitlement?
   "Returns true if the subscription can be entitled to multiple times."
   [username password pool]
-  (let [server (conf-file-value "hostname")
-        port (conf-file-value "port")
-        prefix (conf-file-value "prefix")]
-    (CandlepinTasks/isPoolProductMultiEntitlement server
-                                                  port
-                                                  prefix
-                                                  username
+  (let [url (SubscriptionManagerBaseTestScript/sm_serverUrl)]
+    (CandlepinTasks/isPoolProductMultiEntitlement username
                                                   password
+                                                  url
                                                   pool)))
 
 (defn get-all-facts []
