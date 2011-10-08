562d7faf078dd963f2c828caeb924c9cceb0db85
jsefler <jsefler@redhat.com>
2011-10-07 23:10:39 -0400

    added SubscriptionManager HealingTests that simply toggle a consumer's autoheal attribute

--- src/com/redhat/qe/sm/cli/tests/HealingTests.java
+++ src/com/redhat/qe/sm/cli/tests/HealingTests.java
@@ -1,15 +1,11 @@
 package com.redhat.qe.sm.cli.tests;
 
-import java.util.List;
-
-import org.testng.annotations.AfterGroups;
+import org.json.JSONObject;
 import org.testng.annotations.Test;
 
-import com.redhat.qe.auto.tcms.ImplementsNitrateTest;
 import com.redhat.qe.auto.testng.Assert;
 import com.redhat.qe.sm.base.SubscriptionManagerCLITestScript;
-import com.redhat.qe.sm.data.SubscriptionPool;
-import com.redhat.qe.tools.SSHCommandResult;
+import com.redhat.qe.sm.cli.tasks.CandlepinTasks;
 
 /**
  * @author jsefler
@@ -19,22 +15,36 @@ import com.redhat.qe.tools.SSHCommandResult;
 @Test(groups={"HealingTests"})
 public class HealingTests extends SubscriptionManagerCLITestScript {
 	
-	// There is an autoheal attribute on the consumer
-	
-//	[root@jsefler-onprem-62server tmp]# curl -k -u testuser1:password --request PUT --data '{"autoheal":false}' --header 'accept:application/json' --header 'content-type: application/json' https://jsefler-onprem-62candlepin.usersys.redhat.com:8443/candlepin/consumers/562bbb5b-9645-4eb0-8be8-cd0413d531a7
-//		[root@jsefler-onprem-62server tmp]# 
-//		[root@jsefler-onprem-62server tmp]# curl -k -u testuser1:password --request GET  --header 'accept:application/json' --header 'content-type: application/json' https://jsefler-onprem-62candlepin.usersys.redhat.com:8443/candlepin/consumers/562bbb5b-9645-4eb0-8be8-cd0413d531a7 | python -mjson.tool |  grep heal
-//		  % Total    % Received % Xferd  Average Speed   Time    Time     Time  Current
-//		                                 Dload  Upload   Total   Spent    Left  Speed
-//		100 13921    0 13921    0     0  64077      0 --:--:-- --:--:-- --:--:-- 99435
-//		    "autoheal": false, 
-//		[root@jsefler-onprem-62server tmp]# 
-
-
-
 	// Test methods ***********************************************************************
 	
+	@Test(	description="a new system consumer's autoheal attribute defaults to true (on)",
+			groups={},
+			enabled=true)
+	//@ImplementsNitrateTest(caseId=)
+	public void VerifyAutohealAttributeDefaultsToTrueForNewSystemConsumer_Test() throws Exception {
+		
+		// register a new consumer
+		String consumerId = clienttasks.getCurrentConsumerId(clienttasks.register(sm_clientUsername,sm_clientPassword,sm_clientOrg,null,null,null,null,null,(String)null,true,null,null,null,null));
+		
+		JSONObject jsonConsumer = new JSONObject(CandlepinTasks.getResourceUsingRESTfulAPI(sm_clientUsername,sm_clientPassword, sm_serverUrl, "/consumers/"+consumerId));
+		Assert.assertTrue(jsonConsumer.getBoolean("autoheal"), "A new system consumer's autoheal attribute value defaults to true.");
+	}
 	
+	@Test(	description="using the candlepin api, a consumer's autoheal attribute can be toggled off/on",
+			groups={},
+			dependsOnMethods={"VerifyAutohealAttributeDefaultsToTrueForNewSystemConsumer_Test"},
+			enabled=true)
+	//@ImplementsNitrateTest(caseId=)
+	public void VerifyAutohealAttributeCanBeToggledOffForConsumer_Test() throws Exception {
+		
+		// get the current registered consumer's id
+		String consumerId = clienttasks.getCurrentConsumerId();
+		
+		JSONObject jsonConsumer = CandlepinTasks.setAutohealForConsumer(sm_clientUsername,sm_clientPassword, sm_serverUrl, consumerId,false);
+		Assert.assertFalse(jsonConsumer.getBoolean("autoheal"), "A consumer's autoheal attribute value can be toggled off (expected value=false).");
+		jsonConsumer = CandlepinTasks.setAutohealForConsumer(sm_clientUsername,sm_clientPassword, sm_serverUrl, consumerId,true);
+		Assert.assertTrue(jsonConsumer.getBoolean("autoheal"), "A consumer's autoheal attribute value can be toggled on (expected value=true).");
+	}
 	
 	
 	// Configuration methods ***********************************************************************
