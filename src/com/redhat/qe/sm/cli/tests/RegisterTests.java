792e023bbfc6bbe146563ca36bc30f49b60e25af
John Sefler <jsefler@redhat.com>
2011-12-09 17:05:23 -0500

    added some TODO notes for translation

--- src/com/redhat/qe/sm/cli/tests/RegisterTests.java
+++ src/com/redhat/qe/sm/cli/tests/RegisterTests.java
@@ -989,7 +989,34 @@ Expected Results:
 	    pofile.save(path)
 	    */
 
+		/* TODO Here is a script from alikins that will report untranslated strings
+		#!/usr/bin/python
 		
+		# NEEDS polib from http://pypi.python.org/pypi/polib
+		# or easy_install polib
+		 
+		import glob
+		import polib
+		 
+		#FIXME
+		PO_PATH = "po/"
+		 
+		po_files = glob.glob("%s/*.po" % PO_PATH)
+		 
+		for po_file in po_files:
+		  print
+		  print po_file
+		  p = polib.pofile(po_file)
+		  for entry in p.untranslated_entries():
+		    for line in entry.occurrences:
+		      print "%s:%s" % (line[0], line[1])
+		    print "\t%s" % entry.msgid
+		 
+		  for entry in p.fuzzy_entries():
+		    for line in entry.occurrences:
+		      print "%s:%s" % (line[0], line[1])
+		    print "\t%s" % entry.msgid
+		 */
 		return ll;
 	}
 	
