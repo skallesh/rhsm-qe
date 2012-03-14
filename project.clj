c3e20e942862054ff97dff3037d15e617e9f8eeb
jmolet <jmolet@redhat.com>
2012-03-14 10:03:49 -0400

    final removal of clojure/contrib 1.2

--- project.clj
+++ project.clj
@@ -6,8 +6,6 @@
   :dependencies [[org.clojure/clojure "1.3.0"]
                  [org.clojure/tools.logging "0.2.3"]
                  [org.clojure/data.json "0.1.2"]
-                 ;[org.clojure/clojure "1.2.0"]
-                 ;[org.clojure/clojure-contrib "1.2.0"]
                  [slingshot "0.8.0"]
                  [net.java.dev.rome/rome "1.0.0"]
                  [org.jdom/jdom "1.1"]
