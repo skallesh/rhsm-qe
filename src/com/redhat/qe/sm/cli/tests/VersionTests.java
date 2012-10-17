cf6bb0a57734261f8b019ace10506410d9a38ac9
John Sefler <jsefler@redhat.com>
2012-10-17 08:52:33 -0400

    added a SkipException to VersionOfServerWhenRegisteredAndUsingRHNClassic_Test based on RHEL release

--- src/com/redhat/qe/sm/cli/tests/VersionTests.java
+++ src/com/redhat/qe/sm/cli/tests/VersionTests.java
@@ -141,6 +141,10 @@ public class VersionTests extends SubscriptionManagerCLITestScript {
 			enabled=true)
 	//@ImplementsNitrateTest(caseId=)
 	public void VersionOfServerWhenRegisteredAndUsingRHNClassic_Test() {
+		
+		if (Arrays.asList("6.1","6.2","6.3","5.7","5.8","5.9").contains(clienttasks.redhatReleaseXY)) {
+			throw new SkipException("Blocking bugzilla 852328 was fixed in a subsequent release.  Skipping this test since we already know it will fail in RHEL release '"+clienttasks.redhatReleaseXY+"'.");
+		}
 
 		// make sure we are registered
 		clienttasks.register(sm_clientUsername, sm_clientPassword, sm_clientOrg, null, null, null, null, null, null, null, (List<String>)null, null, null, null, null, null, null, null);
