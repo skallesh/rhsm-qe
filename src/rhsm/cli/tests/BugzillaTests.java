22f3fe94a082e45adaa99dc09201aad252142c6b
Shwetha Kallesh <skallesh@skallesh.eng.blr.redhat.com>
2012-11-07 11:10:32 +0530

    Verifyautosubscribe_Test

--- src/rhsm/cli/tests/BugzillaTests.java
+++ src/rhsm/cli/tests/BugzillaTests.java
@@ -1432,7 +1432,7 @@ public class BugzillaTests extends SubscriptionManagerCLITestScript {
 
 		List<String> ProductIdBeforeAuto=new ArrayList<String>();
 		List<String> ProductIdAfterAuto=new ArrayList<String>();
-
+		clienttasks.deleteFactsFileWithOverridingValues();
 		clienttasks.register_(sm_clientUsername,sm_clientPassword,sm_clientOrg,null,null,null,null,null,null,null,(String)null,null, null, true,null,null, null, null);
 		clienttasks.subscribeToTheCurrentlyAllAvailableSubscriptionPoolsCollectively();
 		for(InstalledProduct installedProductsBeforeAuto : clienttasks.getCurrentlyInstalledProducts()){
