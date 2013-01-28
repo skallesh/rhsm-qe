88ec1fe75cdd9ec16379259627ce0e2aab5ff0c6
John Sefler <jsefler@redhat.com>
2013-01-28 12:11:59 -0500

    changed detection algorithm for ipaddr in SubscriptionManagerTasks

--- src/rhsm/cli/tasks/SubscriptionManagerTasks.java
+++ src/rhsm/cli/tasks/SubscriptionManagerTasks.java
@@ -114,7 +114,7 @@ public class SubscriptionManagerTasks {
 		super();
 		sshCommandRunner = runner;
 		hostname		= sshCommandRunner.runCommandAndWait("hostname").getStdout().trim();
-		ipaddr			= sshCommandRunner.runCommandAndWait("ifconfig eth0 | grep 'inet addr:' | cut -d: -f2 | sed s/'  Bcast'//g").getStdout().trim();
+		ipaddr			= sshCommandRunner.runCommandAndWait("ip addr show eth0 | grep 'scope global eth0' | cut -d/ -f1 | sed s/inet//g").getStdout().trim();
 		arch			= sshCommandRunner.runCommandAndWait("uname --machine").getStdout().trim();  // uname -i --hardware-platform :print the hardware platform or "unknown"	// uname -m --machine :print the machine hardware name
 		releasever		= sshCommandRunner.runCommandAndWait("rpm -q --qf \"%{VERSION}\\n\" --whatprovides /etc/redhat-release").getStdout().trim();  // e.g. 5Server		// cut -f 5 -d : /etc/system-release-cpe	// rpm -q --qf "%{VERSION}\n" --whatprovides system-release		// rpm -q --qf "%{VERSION}\n" --whatprovides /etc/redhat-release
 //		rhsmComplianceD	= sshCommandRunner.runCommandAndWait("rpm -ql subscription-manager | grep libexec/rhsm").getStdout().trim();
@@ -141,6 +141,9 @@ public class SubscriptionManagerTasks {
 		} else {
 			sockets = sshCommandRunner.runCommandAndWait("lscpu | grep 'Socket(s)'").getStdout().split(":")[1].trim();	// Socket(s):             2
 		}
+		
+		// assert some properties for this instance
+		Assert.assertTrue(ipaddr.matches("\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}\\.\\d{1,3}"), "Detected ip address '"+ipaddr+"' for client '"+hostname+"' which appears successful.");
 	}
 	
 
