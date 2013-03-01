402bc0c63edd6c4a27048f748c18e3bef5ed695d
James Molet <jmolet@redhat.com>
2013-03-01 13:18:51 -0500

    Changed logic for BZ 911386 for service_level display

--- src/rhsm/gui/tests/subscribe_tests.clj
+++ src/rhsm/gui/tests/subscribe_tests.clj
@@ -333,7 +333,9 @@
   (tasks/skip-dropdown :all-subscriptions-view subscription)
   (let [guiservice (tasks/ui gettextvalue :all-available-support-level-and-type)
         rawservice (get @servicelist subscription)
-        service (str (or (:support_level rawservice) "Not Set") ", " (:support_type rawservice))]
+        service (str (or (:support_level rawservice) "Not Set")
+                     (when (:support_type rawservice)
+                       (str ", " (:support_type rawservice))))]
     (verify (= guiservice service))))
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
