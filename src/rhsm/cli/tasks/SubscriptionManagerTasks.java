f42e6a4fbf56f792a714a87ffddbc344f52529ed
John Sefler <jsefler@redhat.com>
2013-03-28 19:48:23 -0400

    new case

--- src/rhsm/cli/tasks/SubscriptionManagerTasks.java
+++ src/rhsm/cli/tasks/SubscriptionManagerTasks.java
@@ -3527,6 +3527,7 @@ public class SubscriptionManagerTasks {
 			sshCommandResult.getStdout().startsWith("No subscriptions are available") ||		// No subscriptions are available from the pool with id '8a90f8303c98703a013c98715ca80494'.   (Bug 846758)
 			sshCommandResult.getStdout().startsWith("Pool is restricted") ||					// Pool is restricted to virtual guests: '8a90f85734205a010134205ae8d80403'.
 			sshCommandResult.getStdout().startsWith("All installed products are covered") ||	// All installed products are covered by valid entitlements. No need to update subscriptions at this time.
+			sshCommandResult.getStdout().startsWith("No Installed products on system.") ||		// No Installed products on system. No need to attach subscriptions.
 			sshCommandResult.getStdout().startsWith("Unable to entitle consumer")) {			// Unable to entitle consumer to the pool with id '8a90f8b42e3e7f2e012e3e7fc653013e'.: rulefailed.virt.only
 																								// Unable to entitle consumer to the pool with id '8a90f85734160df3013417ac68bb7108'.: Entitlements for awesomeos-virt-4 expired on: 12/7/11 3:43 AM
 			log.warning(sshCommandResult.getStdout().trim());
