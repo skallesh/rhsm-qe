0bd23f4559deb790fea0db12ea317704bd94e190
John Sefler <jsefler@redhat.com>
2013-04-03 12:54:48 -0400

    added sleeps to the PluginTests hoping to help async logging

--- src/rhsm/cli/tests/PluginTests.java
+++ src/rhsm/cli/tests/PluginTests.java
@@ -173,6 +173,7 @@ public class PluginTests extends SubscriptionManagerCLITestScript {
 
 		// register and assert the pre and post hooks are logged
 		String consumerId = clienttasks.getCurrentConsumerId(clienttasks.register(sm_clientUsername,sm_clientPassword,sm_clientOrg));
+		sleep(5000);	// give the plugin hooks a chance to be called; I think this is an async process
 
 
 		// get the tail of the marked rhsm.log file
@@ -245,6 +246,7 @@ public class PluginTests extends SubscriptionManagerCLITestScript {
 
 		// register and assert the pre and post hooks are logged
 		String consumerId = clienttasks.getCurrentConsumerId(clienttasks.register(sm_clientUsername,sm_clientPassword,sm_clientOrg));
+		sleep(5000);	// give the plugin hooks a chance to be called; I think this is an async process
 
 		// get the tail of the marked rhsm.log file
 		//	2013-03-14 13:37:45,277 [DEBUG]  @plugins.py:695 - Running pre_register_consumer_hook in register_consumer1.RegisterConsumerTestPlugin
@@ -328,6 +330,7 @@ public class PluginTests extends SubscriptionManagerCLITestScript {
 		RemoteFileTasks.markFile(client, clienttasks.rhsmLogFile, logMarker);
 
 		String consumerId = clienttasks.getCurrentConsumerId(clienttasks.register(sm_clientUsername,sm_clientPassword,sm_clientOrg));
+		sleep(5000);	// give the plugin hooks a chance to be called; I think this is an async process
 
 		// get the tail of the marked rhsm.log file
 		//	2013-03-14 18:47:39,595 [DEBUG]  @plugins.py:695 - Running post_facts_collection_hook in facts_collection.FactsCollectionTestPlugin
@@ -349,7 +352,7 @@ public class PluginTests extends SubscriptionManagerCLITestScript {
 		logMarker = System.currentTimeMillis()+" Testing verifyEnabledFactsCollectionTestPluginHooksAreCalled_Test...";
 		RemoteFileTasks.markFile(client, clienttasks.rhsmLogFile, logMarker);
 		clienttasks.facts(null,true,null,null,null);
-		sleep(5000);	// give the server a chance to update the facts; I think this is an async process
+		sleep(5000);	// give the plugin hooks a chance to be called; I think this is an async process
 		
 		// assert the expected log calls
 		logTail = RemoteFileTasks.getTailFromMarkedFile(client, clienttasks.rhsmLogFile, logMarker, "Running post_facts_collection_hook").trim();
@@ -419,6 +422,7 @@ public class PluginTests extends SubscriptionManagerCLITestScript {
 
 		// subscribe to a random pool (to generate calls to pre/post hooks)
 		clienttasks.subscribe(null,null,pool.poolId,null,null,null,null,null,null,null,null);
+		sleep(5000);	// give the plugin hooks a chance to be called; I think this is an async process
 
 		// get the tail of the marked rhsm.log file
 		String logTail = RemoteFileTasks.getTailFromMarkedFile(client, clienttasks.rhsmLogFile, logMarker, "Running p").trim();
@@ -499,6 +503,7 @@ public class PluginTests extends SubscriptionManagerCLITestScript {
 
 		// do a yum transaction and assert that the product_id_install hooks are NOT yet called
 		clienttasks.getYumRepolist("all --enableplugin=product-id");
+		sleep(5000);	// give the plugin hooks a chance to be called; I think this is an async process
 
 		// get the tail of the marked rhsm.log file
 		String logTail = RemoteFileTasks.getTailFromMarkedFile(client, clienttasks.rhsmLogFile, logMarker, "Running p").trim();
@@ -542,6 +547,7 @@ public class PluginTests extends SubscriptionManagerCLITestScript {
 
 		// do a yum install of an ha package
 		clienttasks.yumInstallPackage(sm_haPackages.get(0));	// yum -y install ccs
+		sleep(5000);	// give the plugin hooks a chance to be called; I think this is an async process
 
 		// get the tail of the marked rhsm.log file
  		logTail = RemoteFileTasks.getTailFromMarkedFile(client, clienttasks.rhsmLogFile, logMarker, "Running p").trim();
@@ -616,6 +622,8 @@ public class PluginTests extends SubscriptionManagerCLITestScript {
 		if (pools.isEmpty()) throw new SkipException("Cannot randomly pick a pool for subscribing when there are no available pools for testing."); 
 		SubscriptionPool pool = pools.get(randomGenerator.nextInt(pools.size())); // randomly pick a pool
 		clienttasks.subscribe(null,null,pool.poolId,null,null,null,null,null,null,null,null);
+		sleep(5000);	// give the plugin hooks a chance to be called; I think this is an async process
+
 		// get the tail of the marked rhsm.log file
 		String logTail = RemoteFileTasks.getTailFromMarkedFile(client, clienttasks.rhsmLogFile, logMarker, "Running h").trim();
 		
