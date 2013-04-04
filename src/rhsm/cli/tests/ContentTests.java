f219f59ac2ef6d1bd63c401fd409892365e2f158
John Sefler <jsefler@redhat.com>
2013-04-03 23:20:28 -0400

    added blockedByBug-924919 to VerifyRedHatRepoFileIsPurgedOfBlankLinesByYumPlugin_Test

--- src/rhsm/cli/tests/ContentTests.java
+++ src/rhsm/cli/tests/ContentTests.java
@@ -498,7 +498,7 @@ public class ContentTests extends SubscriptionManagerCLITestScript{
 	}
 	
 	@Test(	description="verify redhat.repo file is purged of successive blank lines by subscription-manager yum plugin",
-			groups={"AcceptanceTests","blockedByBug-737145","blockedByBug-838113"},
+			groups={"AcceptanceTests","blockedByBug-737145","blockedByBug-838113","blockedByBug-924919"},
 			enabled=true)
 	//@ImplementsNitrateTest(caseId=) //TODO Find a tcms caseId for
 	public void VerifyRedHatRepoFileIsPurgedOfBlankLinesByYumPlugin_Test() {
