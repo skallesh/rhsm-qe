01425a8ce0a0aca9fba563e941800c89c877d21e
John Sefler <jsefler@redhat.com>
2013-05-09 16:49:20 -0400

    Scratch testing the idea of a TestNGReportHandlerForRHSM and TestNGListenerForRHSM

---
+++ src/rhsm/testng/TestNGListenerForRHSM.java
@@ -0,0 +1,55 @@
+package rhsm.testng;
+
+
+import java.util.logging.Handler;
+import java.util.logging.Logger;
+
+import org.testng.ITestResult;
+
+import com.redhat.qe.auto.testng.TestNGListener;
+
+public class TestNGListenerForRHSM extends TestNGListener {
+	
+	protected TestNGReportHandlerForRHSM testNGReportHandlerForRHSM;
+	public TestNGListenerForRHSM() {
+		super();
+		// TODO Auto-generated constructor stub
+		for (Handler handler : log.getHandlers()) {
+			if (handler instanceof TestNGReportHandlerForRHSM) {
+				testNGReportHandlerForRHSM = (TestNGReportHandlerForRHSM) handler;
+			}
+		}
+	}
+	
+	
+	@Override
+	public void onTestStart(ITestResult result) {
+		super.onTestStart(result);
+		testNGReportHandlerForRHSM.startBuffer();
+	}
+	
+	@Override
+	public void onTestFailedButWithinSuccessPercentage(ITestResult result) {
+		testNGReportHandlerForRHSM.stopBuffer();
+		super.onTestFailedButWithinSuccessPercentage(result);
+	}
+	
+	@Override
+	public void onTestFailure(ITestResult result) {
+		testNGReportHandlerForRHSM.stopBuffer();
+		super.onTestFailure(result);
+	}
+	
+	@Override
+	public void onTestSkipped(ITestResult result) {
+		testNGReportHandlerForRHSM.stopBuffer();
+		super.onTestSkipped(result);
+	}
+
+	@Override
+	public void onTestSuccess(ITestResult result) {
+		testNGReportHandlerForRHSM.clearBuffer();
+		testNGReportHandlerForRHSM.stopBuffer();
+		super.onTestSuccess(result);
+	}
+}
--- src/rhsm/testng/TestNGReportHandlerForRHSM.java
+++ src/rhsm/testng/TestNGReportHandlerForRHSM.java
@@ -1,26 +1,32 @@
 package rhsm.testng;
 
-import java.util.logging.Level;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.logging.LogRecord;
-
-import org.testng.Reporter;
 
 import rhsm.base.SubscriptionManagerBaseTestScript;
 
 import com.redhat.qe.auto.testng.TestNGReportHandler;
-import com.redhat.qe.jul.TestRecords;
 
 public class TestNGReportHandlerForRHSM extends TestNGReportHandler {
 	@Override
-	public void publish(LogRecord record) {
-		if (Boolean.valueOf(SubscriptionManagerBaseTestScript.getProperty("sm.conservativeLogging", "true"))) {
-			super.publish(new LogRecord(Level.WARNING,"Conservative Logging is ON"));
+	public void publish(LogRecord logRecord) {
+		if (buffering) {
+			logRecordBuffer.add(logRecord);
+		} else {
+			super.publish(logRecord);
 		}
-		if (!Boolean.valueOf(SubscriptionManagerBaseTestScript.getProperty("sm.conservativeLogging", "true"))) {
-			super.publish(new LogRecord(Level.WARNING,"Conservative Logging is OFF"));
-		}
-		super.publish(new LogRecord(Level.INFO,"Hello World... About to log:"+record.getMessage()));
-		super.publish(record);
+//		if (Boolean.valueOf(SubscriptionManagerBaseTestScript.getProperty("sm.conservativeTestNGReporting", "false"))) {
+//			super.publish(record);
+//			return;
+//		}
+//		
+//		
+//		if (!Boolean.valueOf(SubscriptionManagerBaseTestScript.getProperty("sm.conservativeTestNGReporting", "true"))) {
+//			super.publish(new LogRecord(Level.WARNING,"Conservative Logging is OFF"));
+//		}
+//		super.publish(new LogRecord(Level.INFO,"Hello World... About to log:"+record.getMessage()));
+//		super.publish(record);
 //		String css_class = record.getLevel().toString();
 //		if (record.getParameters() != null)
 //		for (Object param: record.getParameters()){
@@ -37,5 +43,31 @@ public class TestNGReportHandlerForRHSM extends TestNGReportHandler {
 //		}
 //		//Reporter.log("<div class='" + css_class + "'>"+record.getMessage() + "</div>");
 //		Reporter.log("<div class='" + css_class + "'>"+tagAllUrls(addLineBreaks(escapeAllTags(record.getMessage()))) + "</div>");
+	}
+	
+	
+	
+	private List<LogRecord> logRecordBuffer = new ArrayList<LogRecord>();
+	private boolean buffering=false;
+	
+	public void clearBuffer() {
+		// clear all log records pending in the buffer (these will never be logged)
+		logRecordBuffer.clear();
+	}
+	public void stopBuffer() {
+		// stop the buffer
+		buffering=false;
+		// and publish what has accumulated
+		for (LogRecord logRecord : logRecordBuffer) {
+			this.publish(logRecord);
+		}
+	}
+	public void startBuffer() {
+		// when the user does not want to use TestNGReportHandlerForRHSM, simply don't start buffering
+		if (!Boolean.valueOf(SubscriptionManagerBaseTestScript.getProperty("sm.conservativeTestNGReporting", "false"))) {
+			return;
+		}
+		
+		buffering=true;
 	}
 }
