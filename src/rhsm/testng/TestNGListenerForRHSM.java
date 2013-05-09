ee8ac360bb5153e823d37c415ac449cf77f685b9
John Sefler <jsefler@redhat.com>
2013-05-09 17:56:04 -0400

    testing TestNGListenerForRHSM

--- src/rhsm/testng/TestNGListenerForRHSM.java
+++ src/rhsm/testng/TestNGListenerForRHSM.java
@@ -10,27 +10,17 @@ import com.redhat.qe.auto.testng.TestNGListener;
 
 public class TestNGListenerForRHSM extends TestNGListener {
 	
-<<<<<<< HEAD
 	protected static TestNGReportHandlerForRHSM testNGReportHandlerForRHSM;
-=======
-	protected TestNGReportHandlerForRHSM testNGReportHandlerForRHSM;
->>>>>>> 01425a8ce0a0aca9fba563e941800c89c877d21e
+
 	public TestNGListenerForRHSM() {
 		super();
 		// TODO Auto-generated constructor stub
 		for (Handler handler : log.getHandlers()) {
-<<<<<<< HEAD
 			if (handler.getClass().isInstance(TestNGReportHandlerForRHSM.class)) {
 				testNGReportHandlerForRHSM = (TestNGReportHandlerForRHSM) handler;
 			}
 		}
 		System.out.print("testNGReportHandlerForRHSM= "+testNGReportHandlerForRHSM);
-=======
-			if (handler instanceof TestNGReportHandlerForRHSM) {
-				testNGReportHandlerForRHSM = (TestNGReportHandlerForRHSM) handler;
-			}
-		}
->>>>>>> 01425a8ce0a0aca9fba563e941800c89c877d21e
 	}
 	
 	
