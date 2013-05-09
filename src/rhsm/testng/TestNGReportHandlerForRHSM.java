3388b25ecc9c08b3c0c09cdc7d90defee1ba0843
John Sefler <jsefler@redhat.com>
2013-05-09 19:32:42 -0400

    TestNGReportHandlerForRHSM seems to be working now

--- src/rhsm/testng/TestNGReportHandlerForRHSM.java
+++ src/rhsm/testng/TestNGReportHandlerForRHSM.java
@@ -8,93 +8,71 @@ import rhsm.base.SubscriptionManagerBaseTestScript;
 
 import com.redhat.qe.auto.testng.TestNGReportHandler;
 
+/**
+ * This class is an attempt to avoid The following exception which we have observed after a
+ * very large job completes.
+    <br>Exception in thread "main" java.lang.OutOfMemoryError: Java heap space
+	<br>at java.util.Arrays.copyOf(Arrays.java:2367)
+	<br>at java.lang.AbstractStringBuilder.expandCapacity(AbstractStringBuilder.java:130)
+	<br>at java.lang.AbstractStringBuilder.ensureCapacityInternal(AbstractStringBuilder.java:114)
+	<br>at java.lang.AbstractStringBuilder.append(AbstractStringBuilder.java:480)
+	<br>at java.lang.StringBuffer.append(StringBuffer.java:309)
+	<br>at java.util.regex.Matcher.appendReplacement(Matcher.java:839)
+	<br>at java.util.regex.Matcher.replaceAll(Matcher.java:906)
+	<br>at org.testng.reporters.XMLStringBuffer.toXML(XMLStringBuffer.java:327)
+	<br>at org.testng.reporters.XMLReporter.generateReport(XMLReporter.java:66)
+	<br>at org.testng.TestNG.generateReports(TestNG.java:1089)
+	<br>at org.testng.TestNG.run(TestNG.java:1048)
+	<br>at org.testng.TestNG.privateMain(TestNG.java:1338)
+	<br>at org.testng.TestNG.main(TestNG.java:1307)
+	<br>
+ * The solution used in this class attempts reduce the amount of logging by excluding log records
+ * published from within a test that PASSED.  Afterall, the logs are useful when a test
+ * does not PASS.
+ *
+ * @author jsefler
+ *
+ */
 public class TestNGReportHandlerForRHSM extends TestNGReportHandler {
+	
+	// used as a state machine
+	private List<LogRecord> logRecordBuffer = new ArrayList<LogRecord>();
+	private boolean buffering = false;
+	
 	@Override
 	public void publish(LogRecord logRecord) {
-//		if (buffering) {
-//			logRecordBuffer.add(logRecord);
-//		} else {
-//			super.publish(logRecord);
-//		}
+		
 		// when the user does not want to use TestNGReportHandlerForRHSM, simply publish the logRecord and return
-		if (!Boolean.valueOf(SubscriptionManagerBaseTestScript.getProperty("sm.conservativeTestNGReporting", "false"))) {
+		if (!Boolean.valueOf(SubscriptionManagerBaseTestScript.getProperty("sm.conservativeTestNGReporting", "true"))) {
 			super.publish(logRecord);
 			return;
 		}
 		
+		// Note: The success of the following logic stack depends on the fixed strings below
+		// matching the fixed strings used in com.redhat.qe.auto.testng.TestNGListener.java 
 		
-		if (logRecord.getMessage().startsWith("Test Passed")) {
-			logRecordBuffer.clear();
+		if (logRecord.getMessage().startsWith("Test Passed")) { // must match string used in com.redhat.qe.auto.testng.TestNGListener.onTestSuccess(ITestResult result)
+			logRecordBuffer.clear();	// this is key call that purges all the logs from a passed test
 			buffering = false;
 		}
 		
-		if (logRecord.getMessage().startsWith("Test Failed")) {
+		if (logRecord.getMessage().startsWith("Test Failed")) { // must match string used in com.redhat.qe.auto.testng.TestNGListener.onTestFailure(ITestResult result)  com.redhat.qe.auto.testng.TestNGListener.onTestFailedButWithinSuccessPercentage(ITestResult result)
 			buffering = false;
 		}
 		
-		if (logRecord.getMessage().startsWith("Skipping Test") || logRecord.getMessage().startsWith("Skipping test")) {
+		if (logRecord.getMessage().startsWith("Skipping Test") ||
+			logRecord.getMessage().startsWith("Skipping test")) { // must match string used in com.redhat.qe.auto.testng.TestNGListener.onTestSkipped(ITestResult result)
 			buffering = false;
 		}
 		
 		logRecordBuffer.add(logRecord);
 		if (!buffering) { // publish the entire logRecordBuffer
-			for (LogRecord record : logRecordBuffer) {
-				super.publish(record);
-			}
+			for (LogRecord record : logRecordBuffer) super.publish(record);
 			logRecordBuffer.clear();
 		}
 		
-		if (logRecord.getMessage().startsWith("Starting Test:")) {
+		if (logRecord.getMessage().startsWith("Starting Test:")) { // must match string used in com.redhat.qe.auto.testng.TestNGListener.onTestStart(ITestResult result)
 			buffering = true;
 		}
-//		
-//		
-//		if (!Boolean.valueOf(SubscriptionManagerBaseTestScript.getProperty("sm.conservativeTestNGReporting", "true"))) {
-//			super.publish(new LogRecord(Level.WARNING,"Conservative Logging is OFF"));
-//		}
-//		super.publish(new LogRecord(Level.INFO,"Hello World... About to log:"+record.getMessage()));
-//		super.publish(record);
-//		String css_class = record.getLevel().toString();
-//		if (record.getParameters() != null)
-//		for (Object param: record.getParameters()){
-//		if (param.equals(TestRecords.Style.Banner))
-//		css_class += " banner";
-//		if (param.equals(TestRecords.Style.StartTest))
-//		css_class += " startTest";
-//		if (param.equals(TestRecords.Style.Action))
-//		css_class += " ACTION";
-//		if (param.equals(TestRecords.Style.Asserted))
-//		css_class += " ASSERT";
-//		if (param.equals(TestRecords.Style.AssertFailed))
-//		css_class += " ASSERTFAIL";
-//		}
-//		//Reporter.log("<div class='" + css_class + "'>"+record.getMessage() + "</div>");
-//		Reporter.log("<div class='" + css_class + "'>"+tagAllUrls(addLineBreaks(escapeAllTags(record.getMessage()))) + "</div>");
 	}
-	
-	
-	
-	private List<LogRecord> logRecordBuffer = new ArrayList<LogRecord>();
-	private boolean buffering=false;
-	
-//	public void clearBuffer() {
-//		// clear all log records pending in the buffer (these will never be logged)
-//		logRecordBuffer.clear();
-//	}
-//	public void stopBuffer() {
-//		// stop the buffer
-//		buffering=false;
-//		// and publish what has accumulated
-//		for (LogRecord logRecord : logRecordBuffer) {
-//			this.publish(logRecord);
-//		}
-//	}
-//	public void startBuffer() {
-//		// when the user does not want to use TestNGReportHandlerForRHSM, simply don't start buffering
-//		if (!Boolean.valueOf(SubscriptionManagerBaseTestScript.getProperty("sm.conservativeTestNGReporting", "false"))) {
-//			return;
-//		}
-//		
-//		buffering=true;
-//	}
 }
