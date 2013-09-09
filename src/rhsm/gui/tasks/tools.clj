a9e37a1b709fb24cddada9156aad7786a4b5159b
Sharath Nagaraj Dwaral <fsharath@redhat.com>
2013-09-09 17:44:08 -0400

    trial change

--- src/rhsm/gui/tasks/tools.clj
+++ src/rhsm/gui/tasks/tools.clj
@@ -47,7 +47,8 @@
   []
   (let [arch (.arch @cli-tasks)
         ;; currently due to ldtp, gui testing is only supported on these arches
-        supported-arches '("i386" "i486" "i586" "i686" "x86_64")]
+        ;supported-arches '("i386" "i486" "i586" "i686" "x86_64")]
+        supported-arches '("i386" "i486" "i586" "i686")]
     (if-not (some #(= arch %) supported-arches)
       (throw (SkipException. (str "Arch '" arch "' is not supported for GUI testing."))))))
 
