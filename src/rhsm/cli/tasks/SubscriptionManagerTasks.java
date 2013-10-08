4d66eb970f40b221be43ad1c42da4b250c183965
John Sefler <jsefler@redhat.com>
2013-10-08 12:35:26 -0400

    another update to detect ip address

--- src/rhsm/cli/tasks/SubscriptionManagerTasks.java
+++ src/rhsm/cli/tasks/SubscriptionManagerTasks.java
@@ -124,9 +124,9 @@ public class SubscriptionManagerTasks {
 		sshCommandRunner = runner;
 		hostname		= sshCommandRunner.runCommandAndWait("hostname").getStdout().trim();
 		sshCommandRunner.runCommandAndWait("ip addr show");
-		//ipaddr			= sshCommandRunner.runCommandAndWait("for IF in $(ip link show | grep 'state UP' | cut -f2 -d':' | sed 's/ //'); do ip addr show $IF | egrep 'scope global .*'$IF | cut -d'/' -f1 | sed 's/ *inet *//g'; done;").getStdout().trim();
 		//ipaddr			= sshCommandRunner.runCommandAndWait("ip addr show | egrep 'scope global (dynamic )?eth' | cut -d/ -f1 | sed s/inet//g").getStdout().trim();
-		ipaddr			= sshCommandRunner.runCommandAndWait("ip addr show | egrep 'scope global (dynamic )?e' | cut -d/ -f1 | sed s/inet//g").getStdout().trim();
+		//ipaddr			= sshCommandRunner.runCommandAndWait("ip addr show | egrep 'scope global (dynamic )?e' | cut -d/ -f1 | sed s/inet//g").getStdout().trim();
+		ipaddr			= sshCommandRunner.runCommandAndWait("for DEVICE in $(ip addr show | egrep 'state (UP|UNKNOWN)' | cut -f2 -d':' | sed 's/ //'); do ip addr show $DEVICE | egrep 'scope global .*'$DEVICE | cut -d'/' -f1 | sed 's/ *inet *//g'; done;").getStdout().trim();	// state is UNKNOWN on ppc64
 		arch			= sshCommandRunner.runCommandAndWait("uname --machine").getStdout().trim();  // uname -i --hardware-platform :print the hardware platform or "unknown"	// uname -m --machine :print the machine hardware name
 		releasever		= sshCommandRunner.runCommandAndWait("rpm -q --qf \"%{VERSION}\\n\" --whatprovides /etc/redhat-release").getStdout().trim();  // e.g. 5Server		// cut -f 5 -d : /etc/system-release-cpe	// rpm -q --qf "%{VERSION}\n" --whatprovides system-release		// rpm -q --qf "%{VERSION}\n" --whatprovides /etc/redhat-release
 //		rhsmComplianceD	= sshCommandRunner.runCommandAndWait("rpm -ql subscription-manager | grep libexec/rhsm").getStdout().trim();
