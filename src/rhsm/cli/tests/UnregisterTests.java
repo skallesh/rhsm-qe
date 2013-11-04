2c55510d11c247a067c449f77c6c29a0c4820be5
John Sefler <jsefler@redhat.com>
2013-11-04 18:53:11 -0500

    updated UnregisterShouldNotThrowUnauthorizedRequests_Test after python-rhsm change to "Log the new requestUuid from candlepin if it is present in the response."

--- src/rhsm/cli/tests/UnregisterTests.java
+++ src/rhsm/cli/tests/UnregisterTests.java
@@ -51,20 +51,19 @@ public class UnregisterTests extends SubscriptionManagerCLITestScript {
 		RemoteFileTasks.markFile(client, clienttasks.rhsmLogFile, logMarker);
 		clienttasks.unregister(null, null, null);
 		//	[root@jsefler-6 ~]# grep -A1 "Making request" /var/log/rhsm/rhsm.log
-		//	2013-09-16 11:53:25,926 [DEBUG]  @connection.py:441 - Making request: GET /subscription/
-		//	2013-09-16 11:53:26,331 [DEBUG]  @connection.py:460 - Response status: 200
-		//	--
 		//	2013-09-16 11:53:26,339 [DEBUG]  @connection.py:441 - Making request: DELETE /subscription/consumers/71775a6e-40e0-4422-8db6-5bff074389ef
 		//	2013-09-16 11:53:27,045 [DEBUG]  @connection.py:460 - Response status: 204
-		//	--
-		//	2013-09-16 11:53:27,247 [DEBUG]  @connection.py:441 - Making request: GET /subscription/consumers/71775a6e-40e0-4422-8db6-5bff074389ef/compliance
-		//	2013-09-16 11:53:27,665 [DEBUG]  @connection.py:460 - Response status: 401
-		//	--
-		//	2013-09-16 11:53:27,678 [DEBUG]  @connection.py:441 - Making request: PUT /subscription/consumers/71775a6e-40e0-4422-8db6-5bff074389ef
-		String logTail = RemoteFileTasks.getTailFromMarkedFile(client, clienttasks.rhsmLogFile, logMarker, "Response status:").trim();
+		
+		//	[root@jsefler-7 ~]# grep -A1 "Making request" /var/log/rhsm/rhsm.log
+		//	2013-11-04 18:38:43,279 [DEBUG] subscription-manager @connection.py:442 - Making request: DELETE /candlepin/consumers/2f90f0ff-fca5-4425-8f84-6d3d951ac4a7
+		//	2013-11-04 18:38:43,504 [DEBUG] subscription-manager @connection.py:465 - Response: status=204, requestUuid=c68cecf5-3972-494a-b3d1-17d8d044d9b8
+
+		String logTail = RemoteFileTasks.getTailFromMarkedFile(client, clienttasks.rhsmLogFile, logMarker, "Response").trim();
 		String unexpectedLogMessage = "Response status: 401";
+		unexpectedLogMessage = "Response: status=401";	// changed by python-rhsm commit ce3727f73c5ac6f77db5e52027443ec456a5d733 Log the new requestUuid from candlepin if it is present in the response.
 		Assert.assertFalse(logTail.contains(unexpectedLogMessage), "The '"+clienttasks.rhsmLogFile+"' should not encounter unexpected log message '"+unexpectedLogMessage+"' after unregister.");
 		String expectedLogMessage = "Response status: 204";
+		expectedLogMessage = "Response: status=204";	// changed by python-rhsm commit ce3727f73c5ac6f77db5e52027443ec456a5d733 Log the new requestUuid from candlepin if it is present in the response.
 		Assert.assertTrue(logTail.contains(expectedLogMessage), "The '"+clienttasks.rhsmLogFile+"' should encounter expected log message '"+expectedLogMessage+"' after unregister (indicative of a successful DELETE request).");
 	}
 	
