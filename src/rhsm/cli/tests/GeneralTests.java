70cdd8bfd4989d55bb8e733465a51571cc3e7838
John Sefler <jsefler@redhat.com>
2013-11-18 13:41:54 -0500

    bumped the rpm requires version for python-rhsm in VerifyRpmRequireListForSubscriptionManager_Test

--- src/rhsm/cli/tests/GeneralTests.java
+++ src/rhsm/cli/tests/GeneralTests.java
@@ -300,7 +300,7 @@ public class GeneralTests extends SubscriptionManagerCLITestScript{
 					"manual: python-ethtool",
 					"manual: python-iniparse",
 					//"manual: python-rhsm >= 1.9.1-1"/*RHEL65*/,
-					"manual: python-rhsm >= 1.10.3"/*RHEL70*/,
+					"manual: python-rhsm >= 1.10.6"/*RHEL70*/,
 					//"manual: python-simplejson",	// removed by bug 1006748
 					"manual: usermode",
 					"manual: virt-what",
