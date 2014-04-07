0b78928c965c5c66ab0ce9aec120090e392baa90
John Sefler <jsefler@redhat.com>
2014-04-07 17:58:02 -0400

    tolerate labels "Quantity:" or "Available:" in SubscriptionPool

--- src/rhsm/data/SubscriptionPool.java
+++ src/rhsm/data/SubscriptionPool.java
@@ -282,7 +282,7 @@ public class SubscriptionPool extends AbstractCommandLineData {
 		regexes.put("productId",				"SKU:(.*)");		// "Product Id"; changed by Bug 806986
 		regexes.put("contract",					"Contract:(.*)");	// "Contract"; added by Bug 1007580
 		regexes.put("poolId",					"Pool ID:(.*)");	// "Pool Id:(.*)"); changed by Bug 878634
-		regexes.put("quantity",					"Available:(.*)");	// "Quantity:(.*)"); changed by Bug 986971	// "quantity:(.*)"); changed by Bug 612730	// may eventually be changed by Bug 963874
+		regexes.put("quantity",					"(?:Quantity:|Available:)(.*)");	// "Quantity:(.*)"); changed by Bug 986971	// "quantity:(.*)"); changed by Bug 612730	// may eventually be changed by Bug 963874
 		regexes.put("suggested",				"Suggested:(.*)");	// added by Bug 1008557
 		regexes.put("serviceLevel",				"Service Level:(.*)");
 		regexes.put("serviceType",				"Service Type:(.*)");
