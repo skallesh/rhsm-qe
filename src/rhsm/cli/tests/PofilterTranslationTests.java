4ac4988f8f555bb2dd76fb9ae36d4f9b7e3872b2
John Sefler <jsefler@redhat.com>
2014-05-16 14:36:47 -0400

    saved translation ticket request for sub-man 1.11

--- src/rhsm/cli/tests/PofilterTranslationTests.java
+++ src/rhsm/cli/tests/PofilterTranslationTests.java
@@ -72,6 +72,10 @@ import com.redhat.qe.tools.SSHCommandRunner;
  *   	Subscription Manger 1.10.X Translation Request
  *		https://translate.zanata.org/zanata/iteration/view/subscription-manager/1.10.X
  *		https://engineering.redhat.com/rt/Ticket/Display.html?id=276556
+ *
+ *  	Subscription Manger 1.11.X Translation Request
+ *		https://translate.zanata.org/zanata/iteration/view/subscription-manager/1.11.X
+ *		https://engineering.redhat.com/rt/Ticket/Display.html?id=294989
  *   
  *   Translators:
  *   
