a23aad1caf4438f64bcc4d5345d1f5f439493f9a
John Sefler <jsefler@redhat.com>
2014-06-26 14:58:06 -0400

    added more channels to blockedByBug 1105656 - missing a few RHN Classic channel mappings to product certs

--- src/rhsm/cli/tests/MigrationDataTests.java
+++ src/rhsm/cli/tests/MigrationDataTests.java
@@ -2073,6 +2073,8 @@ public class MigrationDataTests extends SubscriptionManagerCLITestScript {
 				rhnAvailableChildChannel.equals("rhel-x86_64-server-rhsclient-5-debuginfo") ||
 				rhnAvailableChildChannel.equals("rhel-x86_64-server-5-thirdparty-oracle-java") ||
 				rhnAvailableChildChannel.equals("rhel-x86_64-server-5-thirdparty-oracle-java-beta") ||
+				rhnAvailableChildChannel.equals("rhel-x86_64-client-5-thirdparty-oracle-java") ||
+				rhnAvailableChildChannel.equals("rhel-x86_64-client-5-thirdparty-oracle-java-beta") ||
 				rhnAvailableChildChannel.equals("rhel-x86_64-server-5-cf-me-2")) {
 				// Bug 1105656 - missing a few RHN Classic channel mappings to product certs
 				bugIds.add("1105656");
