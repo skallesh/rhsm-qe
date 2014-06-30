36528af7280237780d1cfbccd726c8c8685030d0
John Sefler <jsefler@redhat.com>
2014-06-30 18:12:07 -0400

    added VerifySubscriptionManagementEntitlementsInContainerMode_Test

--- src/rhsm/cli/tests/DockerTests.java
+++ src/rhsm/cli/tests/DockerTests.java
@@ -35,6 +35,7 @@ import rhsm.data.OrderNamespace;
 import rhsm.data.ProductCert;
 import rhsm.data.ProductNamespace;
 import rhsm.data.SubscriptionPool;
+import rhsm.data.YumRepo;
 
 import com.redhat.qe.tools.RemoteFileTasks;
 import com.redhat.qe.tools.SSHCommandResult;
@@ -150,11 +151,80 @@ public class DockerTests extends SubscriptionManagerCLITestScript {
 		}
 	}
 	
-	protected final String rhsmHostDir = "/etc/rhsm-host";
-	protected final String entitlementHostDir = "/etc/pki/entitlement-host";
 	
 	
 	
+	
+	@Test(	description="Verify that when in container mode, redhat.repo is populated from the entitlements in /etc/rhsm/entitlement-host",
+			groups={"VerifySubscriptionManagementEntitlementsInContainerMode_Test"},
+			enabled=true)
+	//@ImplementsNitrateTest(caseId=)
+	public void VerifySubscriptionManagementEntitlementsInContainerMode_Test() {
+		
+		// start by registering the host with autosubscribe to gain some entitlements...
+		log.info("Start fresh by registering the host with autosubscribe and getting the host's yum repolist...");
+		consumerId = clienttasks.getCurrentConsumerId(clienttasks.register(sm_clientUsername,sm_clientPassword,sm_clientOrg,null,null,null,null,true,null,null,(String)null,null,null, null, true, false, null, null, null));
+		
+		// remember the yum repolist and the subscribed YumRepo data on the host
+		List<YumRepo> subscribedYumReposOnHost = clienttasks.getCurrentlySubscribedYumRepos();
+		if (subscribedYumReposOnHost.isEmpty()) throw new SkipException("Skipping this test when no redhat.repo content is granted.  (Expected autosubscribe to grant some entitlements.)");
+		List<String> yumRepolistOnHost = clienttasks.getYumRepolist("all");
+		if (yumRepolistOnHost.isEmpty()) throw new SkipException("Skipping this test when yum repolist all is empty.  (Should always pass since the prior assert for some granted entitlements passed.)");
+		
+		// put the system into container mode
+		setupContainerMode();
+		
+		// verify that no content is available when /etc/pki/entitlement-host is empty
+		String entitlementCertDir = clienttasks.getConfFileParameter(clienttasks.rhsmConfFile, "rhsm", "entitlementCertDir");
+		client.runCommandAndWait("ls -l "+entitlementCertDir);
+		client.runCommandAndWait("ls -l "+entitlementHostDir);
+		List<String> yumRepolistOnContainer = clienttasks.getYumRepolist("all");
+		Assert.assertTrue(yumRepolistOnContainer.size()<yumRepolistOnHost.size(),"When in container mode (with *no* entitlements in '"+entitlementHostDir+"'), the number of yum repolists available should have dimmished (by the number of redhat.repo repos on the host)");
+		List<YumRepo> subscribedYumReposOnContainer = clienttasks.getCurrentlySubscribedYumRepos();
+		Assert.assertTrue(subscribedYumReposOnContainer.isEmpty(),"When in container mode (with *no* entitlements in '"+entitlementHostDir+"'), there should be no redhat.repo content available.");
+
+		// put the host's entitlements into /etc/pki/entitlement-host
+		client.runCommandAndWait("cp "+entitlementCertDir+"/* "+entitlementHostDir);
+		client.runCommandAndWait("ls -l "+entitlementHostDir);
+		
+		// clean the consumer
+		log.info("Deleting the consumer cert (containers don't depend on a consumer)...");
+		clienttasks.removeAllCerts(true, true, false);
+		
+		// verify that the host entitlements are now accessible when in container mode
+		yumRepolistOnContainer = clienttasks.getYumRepolist("all");
+		Assert.assertTrue(yumRepolistOnContainer.containsAll(yumRepolistOnHost)&&yumRepolistOnHost.containsAll(yumRepolistOnContainer),"When in container mode, the entitlements in '"+entitlementHostDir+"' are reflected in yum repolist all. (yum repolist all in the container matches yum repolist all from the host)");
+		subscribedYumReposOnContainer = clienttasks.getCurrentlySubscribedYumRepos();
+		// Note: the subscribedYumReposOnContainer should only differ from the subscribedYumReposOnHost by the value of the entitlement cert dir path:
+		// sslclientcert = /etc/pki/entitlement-host/2166701319103111701.pem
+		// sslclientkey = /etc/pki/entitlement-host/2166701319103111701-key.pem
+		// sslcacert = /etc/rhsm-host/ca/redhat-uep.pem
+		Assert.assertEquals(subscribedYumReposOnContainer.size(),subscribedYumReposOnHost.size(),"When in container mode, the redhat.repo content in available reflects the same list of redhat.repo content available on the host.  (Size check only.)");
+		for (YumRepo subscribedYumRepoOnHost : subscribedYumReposOnHost) {
+			YumRepo subscribedYumRepoOnContainer = YumRepo.findFirstInstanceWithMatchingFieldFromList("id", subscribedYumRepoOnHost.id, subscribedYumReposOnContainer);
+			Assert.assertEquals(subscribedYumRepoOnContainer.name, subscribedYumRepoOnHost.name,"YumRepo ["+subscribedYumRepoOnHost.id+"] data 'name' compares between host and container entitlements.");
+			Assert.assertEquals(subscribedYumRepoOnContainer.baseurl, subscribedYumRepoOnHost.baseurl,"YumRepo ["+subscribedYumRepoOnHost.id+"] data 'baseurl' compares between host and container entitlements.");
+			if (subscribedYumRepoOnContainer.gpgkey!=null)			Assert.assertEquals(subscribedYumRepoOnContainer.gpgkey, subscribedYumRepoOnHost.gpgkey,"YumRepo ["+subscribedYumRepoOnHost.id+"] data 'gpgkey' compares between host and container entitlements.");
+			if (subscribedYumRepoOnContainer.gpgcheck!=null)		Assert.assertEquals(subscribedYumRepoOnContainer.gpgcheck, subscribedYumRepoOnHost.gpgcheck,"YumRepo ["+subscribedYumRepoOnHost.id+"] data 'gpgcheck' compares between host and container entitlements.");
+			if (subscribedYumRepoOnContainer.enabled!=null)			Assert.assertEquals(subscribedYumRepoOnContainer.enabled, subscribedYumRepoOnHost.enabled,"YumRepo ["+subscribedYumRepoOnHost.id+"] data 'enabled' compares between host and container entitlements.");
+			if (subscribedYumRepoOnContainer.metadata_expire!=null)	Assert.assertEquals(subscribedYumRepoOnContainer.metadata_expire, subscribedYumRepoOnHost.metadata_expire,"YumRepo ["+subscribedYumRepoOnHost.id+"] data 'metadata_expire' compares between host and container entitlements.");
+			// TODO could continue adding more asserts for field equality like these ^
+			Assert.assertTrue(subscribedYumRepoOnContainer.sslclientcert.replaceFirst(entitlementHostDir, "").equals(subscribedYumRepoOnHost.sslclientcert.replaceFirst(entitlementCertDir, "")),"YumRepo ["+subscribedYumRepoOnHost.id+"] data 'sslclientcert' between host '"+subscribedYumRepoOnHost.sslclientcert+"' and container '"+subscribedYumRepoOnContainer.sslclientcert+"' entitlements differ only by directory path.");
+			Assert.assertTrue(subscribedYumRepoOnContainer.sslclientkey.replaceFirst(entitlementHostDir, "").equals(subscribedYumRepoOnHost.sslclientkey.replaceFirst(entitlementCertDir, "")),"YumRepo ["+subscribedYumRepoOnHost.id+"] data 'sslclientkey' between host '"+subscribedYumRepoOnHost.sslclientkey+"' and container '"+subscribedYumRepoOnContainer.sslclientkey+"' entitlements differ only by directory path.");
+			Assert.assertTrue(subscribedYumRepoOnContainer.sslcacert.replaceFirst(rhsmHostDir, "").equals(subscribedYumRepoOnHost.sslcacert.replaceFirst("/etc/rhsm", "")),"YumRepo ["+subscribedYumRepoOnHost.id+"] data 'sslcacert' between host '"+subscribedYumRepoOnHost.sslcacert+"' and container '"+subscribedYumRepoOnContainer.sslcacert+"' entitlements differ only by directory path.");
+		}
+	}
+	@AfterGroups(groups={"setup"},value="VerifySubscriptionManagementEntitlementsInContainerMode_Test")
+	public void teardownVerifySubscriptionManagementEntitlementsInContainerMode_Test() {
+		teardownContainerMode();
+		if (consumerId!=null) {
+			clienttasks.register_(sm_clientUsername,sm_clientPassword,sm_clientOrg,null,null,null,consumerId,null,null,null,(String)null,null,null, null, true, null, null, null, null);
+			clienttasks.unregister_(null, null, null);
+			consumerId=null;
+		}
+	}
+	protected String consumerId=null;
+
 	
 	
 	
@@ -175,8 +245,10 @@ public class DockerTests extends SubscriptionManagerCLITestScript {
 	
 	
 	// Protected methods ***********************************************************************
-
-
+	protected final String rhsmHostDir = "/etc/rhsm-host";
+	protected final String entitlementHostDir = "/etc/pki/entitlement-host";
+	
+	
 	
 	// Data Providers ***********************************************************************
 
