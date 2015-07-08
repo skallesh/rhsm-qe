ae2856ba9934b71f2f86f5ecf116df4aca29e389
John Sefler <jsefler@redhat.com>
2015-07-08 17:06:55 -0400

    added blockedByBug-1241247 to verifyRhsmcertdDoesNotThrowDeprecationWarnings_Test

--- src/rhsm/cli/tests/GeneralTests.java
+++ src/rhsm/cli/tests/GeneralTests.java
@@ -78,7 +78,7 @@ public class GeneralTests extends SubscriptionManagerCLITestScript{
 	
 	
 	@Test(	description="assert the exit code from service rhsmcertd status when running and stopped",
-			groups={"AcceptanceTests","Tier1Tests","blockedByBug-913118","blockedByBug-912707","blockedByBug-914113"})
+			groups={"AcceptanceTests","Tier1Tests","blockedByBug-913118","blockedByBug-912707","blockedByBug-914113","blockedByBug-1241247"})
 	protected void verifyRhsmcertdDoesNotThrowDeprecationWarnings_Test() throws JSONException, Exception {
 		clienttasks.unregister(null, null, null);
 		String marker = System.currentTimeMillis()+" Testing verifyRhsmcertdDoesNotThrowDeprecationWarnings_Test...";
