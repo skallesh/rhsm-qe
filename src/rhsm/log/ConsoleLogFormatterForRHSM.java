33d1a4335fcefb05ecd37886c59bb2c174380afe
John Sefler <jsefler@redhat.com>
2015-08-07 17:35:26 -0400

    added ConsoleLogFormatterForRHSM

---
+++ src/rhsm/log/ConsoleLogFormatterForRHSM.java
@@ -0,0 +1,44 @@
+package rhsm.log;
+
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.logging.Formatter;
+import java.util.logging.LogRecord;
+
+import com.redhat.qe.jul.TestRecords;
+
+/**
+ * This class was copied and modified from com.redhat.qe.jul.ConsoleLogFormatter
+ */
+public class ConsoleLogFormatterForRHSM extends Formatter {
+
+	private static final DateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss.SSS");
+	
+	@Override
+	public String format(LogRecord record) {
+		String date = sdf.format(new Date(record.getMillis()));
+		String throwable = "";
+		String message = record.getMessage();
+		if (record.getThrown() != null) throwable = throwableToString(record.getThrown())  + "\n";
+		if (record.getParameters() != null)
+			for (Object param: record.getParameters()){
+				if (param.equals(TestRecords.Style.Banner))
+					message = "======= " + message;
+			}
+		
+//		return date + " - " + record.getLevel() + ": " + message + " (" + record.getSourceClassName() + "." + record.getSourceMethodName() + ")\n" + throwable;
+		
+		return date + "  " + record.getLevel() + ": " + message + "\n" + throwable;
+	}
+
+	
+	protected String throwableToString(Throwable t){
+		StringWriter sw = new StringWriter();
+		PrintWriter pw = new PrintWriter(sw);
+		t.printStackTrace(pw);
+		return sw.toString();
+	}
+}
