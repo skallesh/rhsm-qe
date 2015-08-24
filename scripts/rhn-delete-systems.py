974af7745f94519b0cb27741bba5fd1ce2b702f1
James Molet <jmolet@redhat.com>
2015-08-24 15:22:58 -0400

    initial commit of rhn-delete-systems tool

---
+++ scripts/rhn-delete-systems.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python
+
+import socket
+import sys, time
+from xmlrpclib import Server
+from optparse import OptionParser
+from pprint import pprint
+
+parser = OptionParser()
+parser.add_option("-u", "--username", dest="username", help="Username")
+parser.add_option("-p", "--password", dest="password", help="Password")
+parser.add_option("-s", "--serverurl", dest="serverurl", help="URL of Satellite Server", default="http://rhsm-sat5.usersys.redhat.com")
+
+(options, args) = parser.parse_args()
+
+if not options.username or not options.password or len(args) < 1:
+   parser.print_usage()
+   sys.exit(1)
+
+# set a socket timeout of 3 minutes; attempt to avoid a 502 Proxy Error after 2 minutes
+socket.setdefaulttimeout(60*3)
+
+# create an api connection to the server
+# RHN API documentation: https://access.stage.redhat.com/knowledge/docs/Red_Hat_Network/
+client = Server("%s/rpc/api/" % options.serverurl)
+#sessionKey = client.auth.login(options.username, options.password)
+sessionKey = None
+count = 0
+while (sessionKey == None):
+    if count > 10:
+        print "Giving up trying to authenticate to RHN API..."
+        sys.exit(-1)
+    try:
+        sessionKey = client.auth.login(options.username, options.password)
+    except Exception, e:
+        print "Unexpected error:", e
+        count += 1
+        time.sleep(10)
+
+systems = client.system.listSystems(sessionKey)
+system_ids = [x['id'] for x in systems if str(x['id']) in args]
+print "Deleting the following systems:"
+pprint(system_ids)
+client.auth.deleteSystems(sessionKey, system_ids)
+client.auth.logout(sessionKey)
+
