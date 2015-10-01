c16c2932b20c7c530bfe2b138a9701781dc35f48
John Sefler <jsefler@redhat.com>
2015-10-01 15:43:00 -0400

    added a comment to PofilterTests

--- src/rhsm/cli/tests/PofilterTranslationTests.java
+++ src/rhsm/cli/tests/PofilterTranslationTests.java
@@ -57,6 +57,7 @@ import com.redhat.qe.tools.SSHCommandRunner;
  *     MSGID="shows pools which provide products that are not already covered"; for L in `rpm -ql subscription-manager | grep rhsm.mo`; do echo ""; echo "Verifying translation for '$MSGID' in LANG file '$L'..."; msgunfmt --no-wrap $L | grep -i "$MSGID" -A1; done;
  *     for L in en_US de_DE es_ES fr_FR it_IT ja_JP ko_KR pt_BR ru_RU zh_CN zh_TW as_IN bn_IN hi_IN mr_IN gu_IN kn_IN ml_IN or_IN pa_IN ta_IN te_IN; do echo ""; echo "# LANG=$L.UTF-8 subscription-manager --help | grep -- --help"; LANG=$L.UTF-8 subscription-manager  --help | grep -- --help; done;
  *	   for F in $(rpm -ql subscription-manager | grep rhsm.mo); do echo ""; echo "$F"; msgunfmt --no-wrap $F | grep msgid | wc -l; done;
+ *     for F in $(rpm -ql subscription-manager | grep rhsm.mo); do echo ""; echo $F; msgunfmt --no-wrap $F >> $F.po; pofilter -t startwhitespace $F.po; done;
  *
  *   Passing LANG through curl:
  *   	Use a hyphenated lang like  de-DE  it-IT  pt-BR  zh-CN
