92e38dcc25e9d914c1d9e756b280fba1704ea46a
John Sefler <jsefler@redhat.com>
2015-12-01 17:49:14 -0500

    updated expected stderr assertion in VerifyIdentityIsBackedUpWhenConsumerIsDeletedServerSide_Test post bug 1262989

--- src/rhsm/cli/tests/IdentityTests.java
+++ src/rhsm/cli/tests/IdentityTests.java
@@ -400,6 +400,7 @@ public class IdentityTests extends SubscriptionManagerCLITestScript {
 		result = clienttasks.list_(null, true, null, null, null, null, null, null, null, null, null, null, null);
 		if (clienttasks.isPackageVersion("subscription-manager",">=","1.16.4-1")) expectedExitCode=new Integer(69/*EX_UNAVAILABLE*/);	// post commit 616ecda6db6ae8b054d7bbb8ba278bba242f4fd0 bug 1262989
 		if (clienttasks.isPackageVersion("subscription-manager",">=","1.16.3-1")) expectedMsg  = String.format("This consumer's profile has been deleted from the server. You can use command clean or unregister to remove local profile.",consumerCert.consumerid);	// post commit 5c48d059bb07b64b92722f249b38aaee7219ab47 bug 1262989
+		if (clienttasks.isPackageVersion("subscription-manager",">=","1.16.5-1")) expectedMsg  = String.format("Consumer profile \"%s\" has been deleted from the server. You can use command clean or unregister to remove local profile.",consumerCert.consumerid);	// post commit 3ad13c20f6ab34cf2621bc48cdd7d15a82791d4f bug 1262989
 		Assert.assertEquals(result.getExitCode(),expectedExitCode,	"Exitcode expected after the consumer has been deleted on the server-side.");
 		if (clienttasks.isPackageVersion("subscription-manager",">=","1.13.9-1")) {	// post commit a695ef2d1da882c5f851fde90a24f957b70a63ad
 			Assert.assertEquals(result.getStdout().trim(),"",			"Stdout expected after the consumer has been deleted on the server-side.");
