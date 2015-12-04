0cb0372e3086a8322359733dc73cbfea78b71c73
Shwetha <skallesh@redhat.com>
2015-12-04 21:40:59 +0530

    adding dataprovider

--- src/rhsm/cli/tests/StorageBandTests.java
+++ src/rhsm/cli/tests/StorageBandTests.java
@@ -1,61 +1,21 @@
 package rhsm.cli.tests;
-import java.io.File;
-import java.io.IOException;
-import java.math.BigInteger;
-import java.text.DateFormat;
-import java.text.ParseException;
-import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Calendar;
-import java.util.Date;
-import java.util.GregorianCalendar;
 import java.util.HashMap;
-import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
-import java.util.Set;
-import java.util.TimeZone;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
 
-import org.apache.xmlrpc.XmlRpcException;
-import org.json.JSONArray;
 import org.json.JSONException;
-import org.json.JSONObject;
-import org.testng.SkipException;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.AfterGroups;
 import org.testng.annotations.BeforeClass;
-import org.testng.annotations.BeforeGroups;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
-
-import com.redhat.qe.Assert;
-import com.redhat.qe.auto.bugzilla.BzChecker;
-import com.redhat.qe.auto.tcms.ImplementsNitrateTest;
-import com.redhat.qe.auto.testng.TestNGUtils;
-
-import rhsm.base.CandlepinType;
-import rhsm.base.ConsumerType;
-import rhsm.base.SubscriptionManagerBaseTestScript;
 import rhsm.base.SubscriptionManagerCLITestScript;
 import rhsm.cli.tasks.CandlepinTasks;
-import rhsm.data.ConsumerCert;
-import rhsm.data.ContentNamespace;
-import rhsm.data.EntitlementCert;
 import rhsm.data.InstalledProduct;
-import rhsm.data.OrderNamespace;
-import rhsm.data.ProductCert;
-import rhsm.data.ProductSubscription;
-import rhsm.data.Repo;
-import rhsm.data.RevokedCert;
 import rhsm.data.SubscriptionPool;
-import rhsm.data.YumRepo;
 
-import com.redhat.qe.tools.RemoteFileTasks;
-import com.redhat.qe.tools.SSHCommandResult;
-import com.redhat.qe.tools.SSHCommandRunner;
+import com.redhat.qe.Assert;
+import com.redhat.qe.auto.testng.TestNGUtils;
 /**
  * @author skallesh
  * 
@@ -70,36 +30,44 @@ public class StorageBandTests extends SubscriptionManagerCLITestScript{
 	 * @throws Exception
 	 * @throws JSONException
 	 */
-	@Test(description = "verify if you attach 1 quantity of subscription that covers 256 GB on a system with 512 GB subscription , installed product is partially subscribed", 
-			groups = { "partiallySubscribeStorageBandSubscription"}, enabled = true)
-	public void partiallySubscribeStorageBandSubscription(){
+	@Test(description = "verify if you attach 1 quantity of subscriptions,each covering 256 GB on a system with 1024 GB subscription , installed product is partially subscribed", 
+			groups = { "partiallySubscribeStorageBandSubscription"},dataProvider="getStorageBandSubscriptions", enabled = true)
+	public void partiallySubscribeStorageBandSubscription(Object Bugzilla,SubscriptionPool storagebandpool) throws JSONException, Exception{
 		clienttasks.register(sm_clientUsername, sm_clientPassword,
 				sm_clientOrg, null, null, null, null, null, null, null,
 				(String) null, null, null, null, true, null, null, null, null);
-		for(SubscriptionPool pool :getStorageBandSubscriptions()){
-			clienttasks.subscribe(null, null, pool.poolId, null, null, "1", null, null, null, null, null, null);
-		}	
-		InstalledProduct installedProduct = InstalledProduct.findFirstInstanceWithMatchingFieldFromList("productId", "908", clienttasks.getCurrentlyInstalledProducts());
-		if(installedProduct.productId.equals("908"))
-			Assert.assertEquals(installedProduct.status, "Partially Subscribed");
-	}
+			clienttasks.subscribe(null, null, storagebandpool.poolId, null, null, "1", null, null, null, null, null, null);
+			
+			List<String> providedProductIds = CandlepinTasks.getPoolProvidedProductIds(sm_clientUsername, sm_clientPassword, sm_serverUrl, storagebandpool.poolId);
+			List<InstalledProduct> installedProducts = clienttasks.getCurrentlyInstalledProducts();
+			for (String providedProductId : providedProductIds) {
+				InstalledProduct installedProduct = InstalledProduct.findFirstInstanceWithMatchingFieldFromList("productId", providedProductId, installedProducts);
+				if (installedProduct!=null) {	
+			Assert.assertEquals(installedProduct.status,"Partially Subscribed","Status of an installed product provided for by a Storage Band entitlement from a pool that covers only 256GB ");	
+		
+				}
+	}}
 	/**
 	 * @author skallesh
 	 * @throws Exception
 	 * @throws JSONException
 	 */
-	@Test(description = "verify if you attach 2 quantity of subscription that covers 256 GB on a system with 512 GB subscription , installed product is fully subscribed", 
-			groups = { "FullySubscribeStorageBandSubscription"}, enabled = true)
-	public void FullySubscribeStorageBandSubscription(){
+	@Test(description = "verify if you attach 2 quantites of subscriptions , each covering that covers 256 GB on a system with 1024 GB subscription , installed product is fully subscribed", 
+			groups = { "FullySubscribeStorageBandSubscription"},dataProvider="getStorageBandSubscriptions", enabled = true)
+	public void FullySubscribeStorageBandSubscription(Object Bugzilla,SubscriptionPool storagebandpool) throws JSONException, Exception{
 		clienttasks.register(sm_clientUsername, sm_clientPassword,
 				sm_clientOrg, null, null, null, null, null, null, null,
 				(String) null, null, null, null, true, null, null, null, null);
-		for(SubscriptionPool pool :getStorageBandSubscriptions()){
-			clienttasks.subscribe(null, null, pool.poolId, null, null, null, null, null, null, null, null, null);
-		}	
-		InstalledProduct installedProduct = InstalledProduct.findFirstInstanceWithMatchingFieldFromList("productId", "908", clienttasks.getCurrentlyInstalledProducts());
-		if(installedProduct.productId.equals("908"))
-			Assert.assertEquals(installedProduct.status, "Subscribed");
+			clienttasks.subscribe(null, null, storagebandpool.poolId, null, null, null, null, null, null, null, null, null);
+		
+			
+			List<String> providedProductIds = CandlepinTasks.getPoolProvidedProductIds(sm_clientUsername, sm_clientPassword, sm_serverUrl, storagebandpool.poolId);
+			List<InstalledProduct> installedProducts = clienttasks.getCurrentlyInstalledProducts();
+			for (String providedProductId : providedProductIds) {
+				InstalledProduct installedProduct = InstalledProduct.findFirstInstanceWithMatchingFieldFromList("productId", providedProductId, installedProducts);
+				if (installedProduct!=null) {	
+			Assert.assertEquals(installedProduct.status.trim()," Subscribed","Status of an installed product provided for by a Storage Band entitlement from a pool that covers only 256GB ");	
+		}}
 	}
 	
 	/**
@@ -108,15 +76,19 @@ public class StorageBandTests extends SubscriptionManagerCLITestScript{
 	 * @throws JSONException
 	 */
 	@Test(description = "verify if you auto-attach , installed product is fully subscribed", 
-			groups = { "AutoAttachStorageBandSubscription"}, enabled = true)
-	public void AutoAttachStorageBandSubscription(){
+			groups = { "AutoAttachStorageBandSubscription"},dataProvider="getStorageBandSubscriptions", enabled = true)
+	public void AutoAttachStorageBandSubscription(Object Bugzilla,SubscriptionPool storagebandpool) throws JSONException, Exception{
 		clienttasks.register(sm_clientUsername, sm_clientPassword,
 				sm_clientOrg, null, null, null, null, null, null, null,
 				(String) null, null, null, null, true, null, null, null, null);
 		clienttasks.subscribe(true, null, (String)null, null, null, null, null, null, null, null, null, null);	
-		InstalledProduct installedProduct = InstalledProduct.findFirstInstanceWithMatchingFieldFromList("productId", "908", clienttasks.getCurrentlyInstalledProducts());
-		if(installedProduct.productId.equals("908"))
-			Assert.assertEquals(installedProduct.status, "Subscribed");
+		List<String> providedProductIds = CandlepinTasks.getPoolProvidedProductIds(sm_clientUsername, sm_clientPassword, sm_serverUrl, storagebandpool.poolId);
+		List<InstalledProduct> installedProducts = clienttasks.getCurrentlyInstalledProducts();
+		for (String providedProductId : providedProductIds) {
+			InstalledProduct installedProduct = InstalledProduct.findFirstInstanceWithMatchingFieldFromList("productId", providedProductId, installedProducts);
+			if (installedProduct!=null) {	
+		Assert.assertEquals(installedProduct.status.trim(),"Subscribed","Status of an installed product provided for by a Storage Band entitlement from a pool that covers only 256GB ");	
+	}}
 	}
 	/**
 	 * @author skallesh
@@ -124,31 +96,57 @@ public class StorageBandTests extends SubscriptionManagerCLITestScript{
 	 * @throws JSONException
 	 */
 	@Test(description = "verify if you auto-heal , installed product is fully subscribed", 
-			groups = { "AutoHealStorageBandSubscription"}, enabled = true)
-	public void AutoHealStorageBandSubscription(){
+			groups = { "AutoHealStorageBandSubscription"}, dataProvider="getStorageBandSubscriptions",enabled = true)
+	public void AutoHealStorageBandSubscription(Object Bugzilla,SubscriptionPool storagebandpool) throws JSONException, Exception{
 		clienttasks.register(sm_clientUsername, sm_clientPassword,
 				sm_clientOrg, null, null, null, null, null, null, null,
 				(String) null, null, null, null, true, null, null, null, null);
 		clienttasks.autoheal(null, true, null, null, null, null);
 		clienttasks.run_rhsmcertd_worker(true);
-		InstalledProduct installedProduct = InstalledProduct.findFirstInstanceWithMatchingFieldFromList("productId", "908", clienttasks.getCurrentlyInstalledProducts());
-		if(installedProduct.productId.equals("908"))
-			Assert.assertEquals(installedProduct.status.trim(), "Subscribed");
+		List<String> providedProductIds = CandlepinTasks.getPoolProvidedProductIds(sm_clientUsername, sm_clientPassword, sm_serverUrl, storagebandpool.poolId);
+		List<InstalledProduct> installedProducts = clienttasks.getCurrentlyInstalledProducts();
+		for (String providedProductId : providedProductIds) {
+			InstalledProduct installedProduct = InstalledProduct.findFirstInstanceWithMatchingFieldFromList("productId", providedProductId, installedProducts);
+			if (installedProduct!=null) {	
+		Assert.assertEquals(installedProduct.status.trim(),"Subscribed","Status of an installed product provided for by a Storage Band entitlement from a pool that covers only 256GB ");	
+	}}
 	}
 	
-	 public List<SubscriptionPool> getStorageBandSubscriptions() {
-		 List<SubscriptionPool> StorageBandPools= new ArrayList<SubscriptionPool>();
-		 for(SubscriptionPool pool:clienttasks.getAvailableSubscriptionsMatchingInstalledProducts()){
-			 if(pool.subscriptionName.toLowerCase().contains("storage") || pool.productId.toLowerCase().contains("storage")){
-				 StorageBandPools.add(pool) ;
-			 }
-		 }
-		 if (StorageBandPools.isEmpty()) throw new SkipException("Could not find any Storage Limited subscription pools/SKUs.");
-		 
-		return StorageBandPools;
-	}
+	
+	 @DataProvider(name="getStorageBandSubscriptions")
+
+	 public Object[][] getStorageBandSubscriptionPoolsDataAs2dArray() throws JSONException,Exception {
+		 System.out.println("Before return statement");
+			return TestNGUtils.convertListOfListsTo2dArray(getStorageBandSubscriptionsPoolsDataAsListOfLists());
+	 }
 	 
-	 
+
+	protected List<List<Object>> getStorageBandSubscriptionsPoolsDataAsListOfLists() throws JSONException, Exception {
+			List<List<Object>> ll = new ArrayList<List<Object>>(); if (!isSetupBeforeSuiteComplete) return ll;
+			
+			for (List<Object> list : getAvailableSubscriptionPoolsDataAsListOfLists(false)) {
+				SubscriptionPool pool = (SubscriptionPool)(list.get(0));
+				if (isPoolBandimited(sm_clientUsername, sm_clientPassword, sm_serverUrl, pool.poolId)) {
+					ll.add(Arrays.asList(new Object[]{null,	pool}));
+				}
+			}
+			return ll;
+		}
+		/**
+		 * A pool that is Storage limited was designed to cover ceph storage
+		 * @param authenticator
+		 * @param password
+		 * @param url
+		 * @param poolId
+		 * @return
+		 * @throws JSONException
+		 * @throws Exception
+		 */
+		public static boolean isPoolBandimited (String authenticator, String password, String url, String poolId) throws JSONException, Exception {
+			String value = CandlepinTasks.getPoolProductAttributeValue(authenticator,password,url,poolId,"storage_band");
+			if (value==null)  return false;
+			return true;
+			}
 	
 	@BeforeClass(groups={"setup"})
 	public void CustomiseCephFacts() throws Exception {
@@ -156,7 +154,7 @@ public class StorageBandTests extends SubscriptionManagerCLITestScript{
 				sm_clientOrg, null, null, null, null, null, null, null,
 				(String) null, null, null, null, true, false, null, null, null);
 		factsMap.clear();
-		factsMap.put("band.storage.usage", "512");
+		factsMap.put("band.storage.usage", "1024");
 		clienttasks.createFactsFileWithOverridingValues(factsMap);
 
 	}
