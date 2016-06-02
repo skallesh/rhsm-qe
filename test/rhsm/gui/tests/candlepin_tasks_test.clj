97d94943c573ec89c89f5ba41ae8aff7ea4bdb16
Jan Stavěl @ RedHat <jstavel@redhat.com>
2016-06-02 16:21:01 +0200

    added test for candlepin_tasks.

---
+++ test/rhsm/gui/tests/candlepin_tasks_test.clj
@@ -0,0 +1,20 @@
+(ns rhsm.gui.tests.candlepin-tasks-test
+  (:require  [clojure.test :refer :all]
+             [rhsm.gui.tests.register_tests :as rtests]
+             [rhsm.gui.tasks.tasks :as t]
+             [rhsm.gui.tasks.candlepin-tasks :as ct]
+             [rhsm.gui.tasks.tools :as tt]
+             [rhsm.gui.tasks.ui :as ui]
+             [rhsm.gui.tasks.test-config :as c]
+             [rhsm.runtestng]
+             [slingshot.slingshot :as sl]
+             [clojure.string :as s]
+             )
+  )
+
+;; initialization of our testware
+(rhsm.runtestng/before-suite true)
+
+(deftest register-with-creds-test
+  (is (contains? #{"Snow White" "Admin Owner"} (ct/get-owner-display-name (:username @c/config) (:password @c/config) (:owner-key @c/config))))
+  )
