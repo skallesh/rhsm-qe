a1b8ee21d30e3a699d0dc41e7cbb2a18bd0d6d1f
Jan Stavěl @ RedHat <jstavel@redhat.com>
2016-06-02 18:06:53 +0200

    added test running register_multi_click test.

--- test/rhsm/gui/tests/register_test.clj
+++ test/rhsm/gui/tests/register_test.clj
@@ -14,11 +14,11 @@
 
 ;; ;; initialization of our testware
 ;; (rhsm.runtestng/before-suite true)
+(rtests/setup nil)
 
 (deftest simple-register-test
   (testing "Simple Register Tests"
     (t/restart-app)
-    (rtests/setup nil)
     (rtests/simple_register nil (:username @c/config) (:password @c/config)
                             (ct/get-owner-display-name (:username @c/config) (:password @c/config) (:owner-key @c/config)))
     )
@@ -26,6 +26,10 @@
 
 (deftest auto_to_register_button-test
   (t/restart-app)
-  (rtests/setup nil)
   (rtests/check_auto_to_register_button nil)
+  )
+
+(deftest register_multi_click-test
+  (t/restart-app)
+  (rtests/register_multi_click nil)
   )
