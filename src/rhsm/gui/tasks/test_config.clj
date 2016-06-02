5435cd2bec1727d1827b5a94339ff7a35a2e1424
Jan Stavěl @ RedHat <jstavel@redhat.com>
2016-06-02 11:21:46 +0200

    added test for owner related properties

--- src/rhsm/gui/tasks/test_config.clj
+++ src/rhsm/gui/tasks/test_config.clj
@@ -63,7 +63,7 @@
                          :server-prefix "sm.server.prefix"
 
                          ;client user information
-                         :owner-key "sm.client1.org"
+                         :owner-key "sm.client1.org.displayName"
                          :password "sm.client1.password"
                          :password1 "sm.client2.password"
                          :username "sm.client1.username"
--- test/rhsm/gui/tests/configuration_test.clj
+++ test/rhsm/gui/tests/configuration_test.clj
@@ -15,48 +15,31 @@
            )
   )
 
-;; (reimport '[rhsm.cli.tasks CandlepinTasks])
 ;; ;; initialization of testing environment
-;; (rhsm.runtestng/before-suite true)
+(rhsm.runtestng/before-suite true)
 
-;; ;; ;; testing of our tests
+;; testing of our testware
+(deftest basic-properties-test
+  (testing "Our java properties are set properly"
+    (testing "- configuration of owner related variables"
+      (is (contains?  #{["snowwhite" "Snow White"]
+                        ["admin" "Admin Owner"]
+                        } (->> ["sm.client1.org" "sm.client1.org.displayName"]
+                             (map #(System/getProperty %))
+                             ))
+       )
+      )
+    )
+  )
 
-;; (deftest basic-configuration-test
-;;   (testing "the first user's credentials must be set"
-;;     (is (not (s/blank? (:username @c/config))))
-;;     (is (not (s/blank? (:password @c/config))))
-;;     (is (not (s/blank? (:owner-key @c/config)))))
-;;   (testing "the second user's credentials must be set "
-;;     (is (not (s/blank? (:username1 @c/config))))
-;;     (is (not (s/blank? (:password1 @c/config)))))
-;;   (comment
-;;     (testing "configuration of database connection"
-;;       (is (not (s/blank? (SubscriptionManagerBaseTestScript/sm_dbHostname)))))
-;;     )
-;;   (comment
-;;     (testing "connection to db exists?"
-;;       (is (not (nil?  (SubscriptionManagerCLITestScript/dbConnection))))))
-;;   )
-
-;; (deftest deploy-candlepin-data-test
-;;   (testing "use proper combination of username and org"
-;;     (let [username (:username @c/config)
-;;           owner-key (:owner-key @c/config)
-;;           ]
-;;       (is (contains?  #{["testuser1" "Snow White"]
-;;                         ["testuser2" "Admin Owner"]
-;;                         } [username owner-key] ))
-;;       )
-;;     )
-;;   )
-
-;; (deftest data-migration-test
-;;   (testing "git repo should exist"
-;;     (let [repo (System/getProperty "sm.rhn.definitionsGitRepository") ]
-;;       (is (or (s/blank? repo)
-;;               (re-find #"\tHEAD" (->> repo (format "git ls-remote %s") tt/run-command  :stdout))
-;;               )
-;;           )
-;;       )
-;;     )
-;;   )
+(deftest deploy-candlepin-data-test
+  (testing "use proper combination of username and org"
+    (let [username (:username @c/config)
+          owner-key (:owner-key @c/config)
+          ]
+      (is (contains?  #{["testuser1" "Snow White"]
+                        ["testuser2" "Admin Owner"]
+                        } [username owner-key] ))
+      )
+    )
+  )
