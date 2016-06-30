24bd027b0d8aa6d2481672b409e9047a51ef49ff
Jan Stavěl @ RedHat <jstavel@redhat.com>
2016-06-30 11:30:20 +0200

    added test of behaviour for "About" window.

--- test/rhsm/gui/tests/facts_test.clj
+++ test/rhsm/gui/tests/facts_test.clj
@@ -13,6 +13,7 @@
              [rhsm.runtestng]
              [slingshot.slingshot :as sl]
              [clojure.string :as s]
+             [clojure.set :refer [subset?]]
              [rhsm.gui.tests.base :as base]))
 
 ;; ;; initialization of our testware
@@ -22,6 +23,21 @@
                       (ftests/register nil)
                       (f)))
 
+
+
+(deftest window-system-registration-is-available-test
+  (testing "Test such that a window 'System Registration' is named as register_dialog for RHEL7"
+    (case (tt/get-release)
+      "RHEL6" (is (= "About Subscription Manager" (-> ui/windows :about-dialog :id)))
+      "RHEL7" (is (= "dlgAboutsubscription-manager-gui" (-> #spy/d ui/windows :about-dialog :id)))
+      "default")))
+
+(deftest list-of-dialogs-after-click-on-about-test
+  (testing "list of windows that appear after I click on 'About' button"
+    (tasks/restart-app)
+    (tasks/ui click :about)
+    (Thread/sleep 1000)
+    (is (subset? #{"dlg0" "frmDesktop" "dlgWarning" "frmTerminal" "frmSubscriptionManager" "dlgAboutsubscription-manager-gui"} (set (tasks/ui getwindowlist))))))
 
 (deftest verify_about_information-test
   (testing "Test for verify_about_information GUI test"
