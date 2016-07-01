96f0a9f8378225b8241223ef802538733529f179
Jan Stavěl @ RedHat <jstavel@redhat.com>
2016-07-01 15:28:06 +0200

    support test for verify_about_information tests.

--- test/rhsm/gui/tests/facts_test.clj
+++ test/rhsm/gui/tests/facts_test.clj
@@ -13,8 +13,10 @@
              [rhsm.runtestng]
              [slingshot.slingshot :as sl]
              [clojure.string :as s]
+             [clojure.core.match :refer [match]]
              [clojure.set :refer [subset?]]
-             [rhsm.gui.tests.base :as base]))
+             [rhsm.gui.tests.base :as base])
+  (:import   org.testng.SkipException))
 
 ;; ;; initialization of our testware
 (use-fixtures :once (fn [f]
@@ -29,7 +31,7 @@
   (testing "Test such that a window 'System Registration' is named as register_dialog for RHEL7"
     (case (tt/get-release)
       "RHEL6" (is (= "About Subscription Manager" (-> ui/windows :about-dialog :id)))
-      "RHEL7" (is (= "dlgAboutsubscription-manager-gui" (-> #spy/d ui/windows :about-dialog :id)))
+      "RHEL7" (is (= "dlgAboutsubscription-manager-gui" (-> ui/windows :about-dialog :id)))
       "default")))
 
 (deftest list-of-dialogs-after-click-on-about-test
@@ -37,8 +39,23 @@
     (tasks/restart-app)
     (tasks/ui click :about)
     (Thread/sleep 1000)
-    (is (subset? #{"dlg0" "frmDesktop" "dlgWarning" "frmTerminal" "frmSubscriptionManager" "dlgAboutsubscription-manager-gui"} (set (tasks/ui getwindowlist))))))
+    (case (tt/get-release)
+      "RHEL6" (is (subset? #{"frmSubscriptionManager" "dlgAboutSubscriptionManager"} (set (tasks/ui getwindowlist))))
+      "RHEL7" (is (subset? #{"frmSubscriptionManager" "dlgAboutsubscription-manager-gui"} (set (tasks/ui getwindowlist))))
+      "default")))
 
 (deftest verify_about_information-test
-  (testing "Test for verify_about_information GUI test"
-    (ftests/verify_about_information nil)))
+  (testing "A test which verifies that a test verify_about_information raises SkipException for newer versions of RHEL"
+    (let [[_ major minor] (re-find #"(\d)\.(\d)" (-> :true tt/get-release :version))]
+      (match [major minor]
+             ["6" _] (ftests/verify_about_information nil)
+             ["7" (a :guard #(>= (Integer. %) 3))] (is (thrown? SkipException (ftests/verify_about_information nil)))
+             :else (ftests/verify_about_information nil)))))
+
+(deftest verify_about_information_without_close_button-test
+  (testing "A test which verifies that a test verify_about_information_without_close_button raises SkipException for older RHEL versions than 7.3"
+    (let [[_ major minor] (re-find #"(\d)\.(\d)" (-> :true tt/get-release :version))]
+      (match [ #spy/d major #spy/d minor]
+             ["6" _] (is (thrown? SkipException (ftests/verify_about_information_without_close_button nil)))
+             ["7" (a :guard #(>= (Integer. %) 3))] (ftests/verify_about_information_without_close_button nil)
+             :else (is (thrown? SkipException (ftests/verify_about_information_without_close_button nil)))))))
