d830ba12b014796dc1127cf8f886d53ff32b8911
Jan Stavěl @ RedHat <jstavel@redhat.com>
2016-11-03 15:35:44 +0100

    aux test and example how to use 'screenshot-at-exception'

--- test/rhsm/gui/tasks/tasks_test.clj
+++ test/rhsm/gui/tasks/tasks_test.clj
@@ -1,8 +1,8 @@
 (ns rhsm.gui.tasks.tasks-test
   (:require  [clojure.test :refer :all]
-             [rhsm.gui.tests.register_tests :as rtests]
-             [rhsm.gui.tasks.tasks :as t]
-             [rhsm.gui.tasks.tools :as tt]
+             [rhsm.gui.tests.register_tests :as  tests]
+             [rhsm.gui.tasks.tasks :as tasks]
+             [rhsm.gui.tasks.tools :as tools]
              [rhsm.gui.tasks.test-config :as c]
              [rhsm.gui.tests.base :as base]
              [rhsm.runtestng]
@@ -16,11 +16,17 @@
                       (f)))
 
 (deftest register-with-creds-test
-  (t/restart-app)
-  (t/register-with-creds))
+  (tasks/restart-app)
+  (tasks/register-with-creds))
 
 (deftest take-screenshot-test
-  (t/take-screenshot "test"))
+  (tasks/take-screenshot "test"))
 
 (deftest verify-or-take-screenshot-test
-  (t/verify-or-take-screenshot false))
+  (tasks/verify-or-take-screenshot false))
+
+(deftest screenshot-on-exception-test
+  (tasks/screenshot-on-exception
+   :default-name
+   (println "some message")
+   (throw+ "error in common way")))
