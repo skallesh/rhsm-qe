f14d6ad3a8d3132244675478e21c79960ffc52ba
Jan Stavěl @ RedHat <jstavel@redhat.com>
2016-11-07 19:04:40 +0100

    added skip-exception type

--- src/rhsm/errors/classification.clj
+++ src/rhsm/errors/classification.clj
@@ -7,13 +7,12 @@
   "Methods related to classifying of exceptions mainly.
   It is important to distinguish errors and exceptions
   from TestNG report results perspective."
-  (urgence_level [a]
+  (urgency_level [a]
     "returns:
       -  :testware-problem
       -  :verification-failure
+      -  :skip-exception
       -  :unknown-exception
-      -  :unknown
-    "
-    )
-  )
+      -  :unknown"))
 
+(gen-class)
