d49adb2e635eeec2493262b53d471ca94097cd09
Jan Stavěl @ RedHat <jstavel@redhat.com>
2016-11-08 18:34:00 +0100

    macro to re-raise an exception in accordance with test result

--- src/rhsm/errors/classification.clj
+++ src/rhsm/errors/classification.clj
@@ -1,5 +1,6 @@
 (ns rhsm.errors.classification
-  (:use [slingshot.slingshot :only (try+ throw+)])
+  (:use [slingshot.slingshot :only (try+ throw+)]
+        [clojure.core.match :only (match)])
   (:require [clojure.tools.logging :as log]))
 
 (defprotocol IFailureClassifier
@@ -15,14 +16,19 @@
       -  :unknown-exception
       -  :unknown"))
 
-;; (defmacro normalize-exception-types
-;;   "This macro catches an exception
-;;   and tries to find out 'failure-level' of the exception.
-;;   After that it raises the proper exception when 'failure-level'
-;;   requires so."
-;;   [& body]
-;;   '(sl/try+
-;;     ~@body
-;;     (catch Object )
-;;     )
-;;   )
+(defmacro normalize-exception-types
+  "This macro catches an exception
+  and tries to find out 'failure-level' of the exception.
+  After that it raises the proper exception when 'failure-level'
+  requires so."
+  [& body]
+  `(try+
+    ~@body
+    (catch Object e#
+      (-> (failure-level e#)
+          (match :verification-failure :re-throw
+                 :network-error        :throw-as-skipped
+                 :else                 :no-throw)
+          (match :throw-as-skipped (throw+ (SkipException.
+                                            (format "Trapped exception: %s" e#)))
+                 :re-throw         (throw+ e#))))))
