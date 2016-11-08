33d97107be9e93fb3e9071ee9d5723df2bf6e7c3
Jan Stavěl @ RedHat <jstavel@redhat.com>
2016-11-08 19:15:36 +0100

    aux tests for AssertionError

--- test/rhsm/errors/classification_test.clj
+++ test/rhsm/errors/classification_test.clj
@@ -1,5 +1,6 @@
 (ns rhsm.errors.classification-test
-  (:use [slingshot.slingshot :only (try+ throw+)])
+  (:use [slingshot.slingshot :only (try+ throw+)]
+        [com.redhat.qe.verify :only (verify)])
   (:require [rhsm.errors.classification :as sut]
             [clojure.test :refer [deftest is]])
   (:import org.testng.SkipException))
@@ -16,6 +17,11 @@
 (deftest unknown-integer-test
   (is (= :unknown (sut/failure-level (Integer. 10)))))
 
+(deftest verification-exception-test
+  (let [e (try+ (verify false)
+                (catch Object e e))]
+    (is (= :verification-failure (sut/failure-level e)))))
+
 (deftest network-candlepin-connection-error-test
   (let [e (try+ (throw+  {:type :network-error
                           :msg "Unable to reach the server at jsefler-candlepin6.usersys.redhat.com:8443/candlepin"
@@ -26,10 +32,14 @@
     (is (= :network-error (sut/failure-level e)))))
 
 (deftest exception-re-risen-test
-  (sut/normalize-exception-types
-   (throw+  {:type :network-error
-             :msg "Unable to reach the server at jsefler-candlepin6.usersys.redhat.com:8443/candlepin"
-             :log-warning :some-object-reference
-             :cancel :some-object-reference})
-   )
-  )
+  (is (thrown? SkipException
+               (sut/normalize-exception-types
+                (throw+  {:type :network-error
+                          :msg "Unable to reach the server at jsefler-candlepin6.usersys.redhat.com:8443/candlepin"
+                          :log-warning :some-object-reference
+                          :cancel :some-object-reference})))))
+
+(deftest verify-error-passes-throught-test
+  (is (thrown? java.lang.AssertionError
+               (sut/normalize-exception-types
+                (verify false)))))
