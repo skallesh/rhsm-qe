70f52a240a8fb3902b2d08096b2f41269eb82f4d
Jan Stavěl @ RedHat <jstavel@redhat.com>
2016-11-11 11:27:30 +0100

    closing of 'repositories-dialog' made safer.

--- src/rhsm/gui/tests/repo_tests.clj
+++ src/rhsm/gui/tests/repo_tests.clj
@@ -220,7 +220,8 @@ Then I see values of repositories ids to be redrawn
         ;; I click twice and I get two different sort orders
         (verify (= (set [:desc-sorting :asc-sorting]) (set [sorting-after-click-01 sorting-after-click-02])))))
     (finally
-      (tasks/ui click :close-repo-dialog)
+      (when (-> (tasks/ui guiexist :repositories-dialog) bool)
+        (tasks/ui click :close-repo-dialog))
       (tasks/unsubscribe_all))))
 
 (defn ^{Test {:groups ["repo"
@@ -237,7 +238,8 @@ Then I see values of repositories ids to be redrawn
     (verify (bool (tasks/ui guiexist :repositories-dialog)))
     (verify (< 0 (tasks/ui getrowcount :repo-table)))
     (finally
-      (tasks/ui click :close-repo-dialog)
+      (when (-> (tasks/ui guiexist :repositories-dialog) bool)
+        (tasks/ui click :close-repo-dialog))
       (tasks/unsubscribe_all))))
 
 (defn ^{Test {:groups ["repo"
@@ -288,7 +290,8 @@ Then I see values of repositories ids to be redrawn
           (sleep 2000)
           (verify (not (tasks/has-state? :repo-remove-override "enabled"))))))
     (finally
-      (tasks/ui click :close-repo-dialog)
+      (when (-> (tasks/ui guiexist :repositories-dialog) bool)
+        (tasks/ui click :close-repo-dialog))
       (tasks/unsubscribe_all))))
 
 (defn toggle-checkbox-state
@@ -465,7 +468,8 @@ Then I see values of repositories ids to be redrawn
                  (tasks/has-state? :repo-remove-override "enabled")))
     (assert-and-remove-all-override)
     (finally
-      (tasks/ui click :close-repo-dialog)
+      (when (-> (tasks/ui guiexist :repositories-dialog) bool)
+        (tasks/ui click :close-repo-dialog))
       (tasks/unsubscribe_all))))
 
 (defn ^{Test {:groups ["repo"
