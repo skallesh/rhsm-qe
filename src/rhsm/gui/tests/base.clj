7c7d4cd1b9a2503749c870b61cc0a670c709ed2f
Jan Stavěl @ RedHat <jstavel@redhat.com>
2016-11-25 16:12:46 +0100

    close/open candlepin connection has 'runner' arg now

--- src/rhsm/gui/tests/base.clj
+++ src/rhsm/gui/tests/base.clj
@@ -55,44 +55,49 @@
   "The function runs 'iptables' to open connection to a server."
   ;; iptables -D OUTPUT -d subscription.rhsm.stage.redhat.com -j REJECT
   ;; iptables: No chain/target/match by that name.
-  []
-  ;; remove all this task related REJECT rules from OUTPUT chain
-  (let [iptables-cmd (format "iptables -D OUTPUT -d %s -j REJECT" (conf-file-value "hostname"))]
-    (loop [result-of-del-cmd (-> iptables-cmd run-command :stderr)
-           iteration 1]
-      (if-not (or (.contains result-of-del-cmd "No chain/target/match by that name.") (>= iteration 10))
-        (recur (-> iptables-cmd run-command :stderr) (inc iteration))))))
+  ([runner]
+   ;; remove all this task related REJECT rules from OUTPUT chain
+   (let [iptables-cmd (format "iptables -D OUTPUT -d %s -j REJECT" (conf-file-value "hostname"))]
+     (loop [result-of-del-cmd (-> iptables-cmd (run-command :runner runner) :stderr)
+            iteration 1]
+       (if-not (or (.contains result-of-del-cmd "No chain/target/match by that name.") (>= iteration 10))
+         (recur (-> iptables-cmd (run-command :runner runner) :stderr) (inc iteration))))))
+  ([] (open-connection-to-candlepin @config/clientcmd)))
 
 (defn close-connection-to-candlepin
   "The function runs 'iptables' to close the connection to a candlepin server."
-  []
-  ;; iptables -A OUTPUT -d subscription.rhsm.stage.redhat.com -j REJECT
-  ;; openssl s_client -debug -connect subscription.rhsm.stage.redhat.com:443
-  ;; Connection refused
-  (let [iptables-cmd (format "iptables -A OUTPUT -d %s -j REJECT" (conf-file-value "hostname"))
-        ;; connection-test (format "openssl s_client -debug -host %s -port %s"
-        ;;                         (conf-file-value "hostname")
-        ;;                         (conf-file-value "port"))
-        ]
-    (run-command iptables-cmd)
-    ;(verify (-> connection-test run-command :stderr (.contains "Connection refused")))
-    ))
+  ([runner]
+   ;; iptables -A OUTPUT -d subscription.rhsm.stage.redhat.com -j REJECT
+   ;; openssl s_client -debug -connect subscription.rhsm.stage.redhat.com:443
+   ;; Connection refused
+   (let [iptables-cmd (format "iptables -A OUTPUT -d %s -j REJECT" (conf-file-value "hostname"))
+         ;; connection-test (format "openssl s_client -debug -host %s -port %s"
+         ;;                         (conf-file-value "hostname")
+         ;;                         (conf-file-value "port"))
+         ]
+     (run-command iptables-cmd :runner runner)
+     ;;(verify (-> connection-test run-command :stderr (.contains "Connection refused")))
+     ))
+  ([] (close-connection-to-candlepin @config/clientcmd)))
 
 (defn ^{BeforeSuite {:groups ["setup"]}}
   startup [_]
   (try
-    (println "----------------------------------- before suite ---------------------------------------")
+    (println "----------------------------------- going to setup suite ---------------------------------------")
     (let [cliscript (SubscriptionManagerCLITestScript.)]
       (.setupBeforeSuite cliscript))
-    (println "----------------------------------- before config init ---------------------------------------")
+    (println "----------------------------------- going to config init ---------------------------------------")
     (config/init)
     (assert-valid-testing-arch)
-    (println "----------------------------------- before update ldtpd ---------------------------------------")
+    (println "----------------------------------- going to update ldtpd ---------------------------------------")
     (update-ldtpd (:ldtpd-source-url @config/config))
-    (println "----------------------------------- before restart vnc ---------------------------------------")
+    (println "----------------------------------- going to restart vnc ---------------------------------------")
     (restart-vnc)
-    (println "----------------------------------- before open connection to candlepin ---------------------------------------")
+    (println "----------------------------------- going to open connection to candlepin on the client machine ---------------------------------------")
     (open-connection-to-candlepin)
+    (println "----------------------------------- going to open connection to candlepin on the proxy server ---------------------------------------")
+    (open-connection-to-candlepin @config/noauth-proxyrunner)
+
     (println "----------------------------------- before connect ---------------------------------------")
     (connect)
     (println "----------------------------------- before mount/start ---------------------------------------")
