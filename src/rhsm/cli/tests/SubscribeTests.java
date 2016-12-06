a9e06521419424488f18ada4907dd07757310549
John Sefler <jsefler@redhat.com>
2016-12-06 17:57:14 -0500

    added another TEMPORARY WORKAROUND block to SubscribeToSubscriptionPoolProductId_Test for candlepin Bug 1394401 - The list of provided products for Temporary Subscriptions is empty

--- src/rhsm/cli/tests/SubscribeTests.java
+++ src/rhsm/cli/tests/SubscribeTests.java
@@ -63,7 +63,8 @@ public class SubscribeTests extends SubscriptionManagerCLITestScript{
 	public void SubscribeToSubscriptionPoolProductId_Test(String productId, JSONArray bundledProductDataAsJSONArray) throws Exception {
 ///*debugTesting*/ if (!productId.equals("awesomeos-ul-quantity-virt")) throw new SkipException("debugTesting - Automator should comment out this line."); 		
 ///*debugTesting*/ if (!productId.equals("awesomeos-onesocketib")) throw new SkipException("debugTesting - Automator should comment out this line."); 		
-///*debugTesting*/ if (!productId.equals("awesomeos-server-basic-dc")) throw new SkipException("debugTesting - Automator should comment out this line."); 		
+///*debugTesting*/ if (!productId.equals("awesomeos-virt-4")) throw new SkipException("debugTesting - Automator should comment out this line."); 		
+///*debugTesting*/ if (!productId.equals("awesomeos-virt-4")&&!productId.equals("awesomeos-ul-quantity-virt")&&!productId.equals("awesomeos-onesocketib")&&!productId.equals("awesomeos-instancebased")) throw new SkipException("debugTesting - Automator should comment out this line."); 		
 ///*debugTesting*/ if (!productId.equals("2cores-2ram-multiattr")) throw new SkipException("debugTesting - Automator should comment out this line."); 		
 ///*debugTesting*/ if (!productId.equals("RH0380468")) throw new SkipException("debugTesting - Automator should comment out this line."); 		
 ///*debugTesting*/ if (!productId.equals("RH00284")) throw new SkipException("debugTesting - Automator should comment out this line."); 		
@@ -227,6 +228,15 @@ public class SubscribeTests extends SubscriptionManagerCLITestScript{
 			for (ProductNamespace pn : entitlementCert.productNamespaces) {
 				if (pn.id.equals(bundledProductId)) productNamespace = pn;
 			}
+			
+			// TEMPORARY WORKAROUND
+			invokeWorkaroundWhileBugIsOpen = true;
+			bugId="1394401"; // Bug 1394401 - The list of provided products for Temporary Subscriptions is empty
+			try {if (invokeWorkaroundWhileBugIsOpen&&BzChecker.getInstance().isBugOpen(bugId)) {log.fine("Invoking workaround for "+BzChecker.getInstance().getBugState(bugId).toString()+" Bugzilla "+bugId+".  (https://bugzilla.redhat.com/show_bug.cgi?id="+bugId+")");SubscriptionManagerCLITestScript.addInvokedWorkaround(bugId);} else {invokeWorkaroundWhileBugIsOpen=false;}} catch (XmlRpcException xre) {/* ignore exception */} catch (RuntimeException re) {/* ignore exception */}
+			if (invokeWorkaroundWhileBugIsOpen && !bundledProductNames.isEmpty() && pool.provides.isEmpty() && pool.subscriptionType.contains("Temporary")) {
+				log.warning("While bug '"+bugId+"' is open, skip assertion of the consumed entitlement provided products amongst the list of install products.");
+			} else
+			// END OF WORKAROUND
 			
 			// assert the installed status of the corresponding product
 			if (entitlementCert.productNamespaces.isEmpty()) {
