0d9722390b2da2d900aed2386a60780d5b843ab8
Jan Stavěl @ RedHat <jstavel@redhat.com>
2016-12-09 10:15:52 +0100

    aux tests for classification updated

--- test/rhsm/errors/classification_test.clj
+++ test/rhsm/errors/classification_test.clj
@@ -1,27 +1,28 @@
 (ns rhsm.errors.classification-test
   (:use [slingshot.slingshot :only (try+ throw+)]
         [com.redhat.qe.verify :only (verify)])
-  (:require [rhsm.errors.classification :as sut]
-            [rhsm.errors.exceptions :as exc]
+  (:require [rhsm.errors.utils :as sut]
+            [rhsm.errors.protocols :as prot]
+            [rhsm.errors.implementations :as impl]
             [clojure.test :refer [deftest is]])
   (:import org.testng.SkipException))
 
 (deftest skip-exception-failure-level-test
-  (is (= :skip-exception (sut/failure-level (new SkipException "some exception")))))
+  (is (= :skip-exception (prot/failure-level (new SkipException "some exception")))))
 
 (deftest exception-test
-  (is (= :unknown-exception (sut/failure-level (new Exception "some exception in general manner")))))
+  (is (= :unknown-exception (prot/failure-level (new Exception "some exception in general manner")))))
 
 (deftest unknown-string-test
-  (is (= :unknown (sut/failure-level "unknown string"))))
+  (is (= :unknown (prot/failure-level "unknown string"))))
 
 (deftest unknown-integer-test
-  (is (= :unknown (sut/failure-level (Integer. 10)))))
+  (is (= :unknown (prot/failure-level (Integer. 10)))))
 
 (deftest verification-exception-test
   (let [e (try+ (verify false)
                 (catch Object e e))]
-    (is (= :verification-failure (sut/failure-level e)))))
+    (is (= :verification-failure (prot/failure-level e)))))
 
 (deftest network-candlepin-connection-error-test
   (let [e (try+ (throw+  {:type :network-error
@@ -30,7 +31,7 @@
                           :cancel :some-object-reference})
                 (catch Object e
                   e))]
-    (is (= :network-error (sut/failure-level e)))))
+    (is (= :network-error (prot/failure-level e)))))
 
 (deftest exception-re-risen-test
   (is (thrown? SkipException
