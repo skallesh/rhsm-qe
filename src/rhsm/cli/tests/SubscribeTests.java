77c04a6041f8d059914eda15d6edb96db35d3143
John Sefler <jsefler@redhat.com>
2016-12-15 20:18:47 -0500

    added a TEMPORARY WORKAROUND to AutoSubscribeOnVirtualSystemsFavorVirtualPools_Test while the following candlepin bug is open...
    Bug 1394401 - The list of provided products for Temporary Subscriptions
    is empty

--- src/rhsm/cli/tests/SubscribeTests.java
+++ src/rhsm/cli/tests/SubscribeTests.java
@@ -1542,6 +1542,17 @@ public class SubscribeTests extends SubscriptionManagerCLITestScript{
 					// get the product certs that are provided by these pools
 					List<String> virtualProductIds = CandlepinTasks.getPoolProvidedProductIds(sm_clientUsername, sm_clientPassword, sm_serverUrl, virtualPool.poolId);
 					List<String> physicalProductIds = CandlepinTasks.getPoolProvidedProductIds(sm_clientUsername, sm_clientPassword, sm_serverUrl, physicalPool.poolId);
+					// TEMPORARY WORKAROUND
+					if (virtualPool.subscriptionType.contains("(Temporary)") && virtualProductIds.isEmpty() && !physicalProductIds.isEmpty()) {
+						boolean invokeWorkaroundWhileBugIsOpen = true;
+						String bugId="1394401"; // Bug 1394401 - The list of provided products for Temporary Subscriptions is empty
+						try {if (invokeWorkaroundWhileBugIsOpen&&BzChecker.getInstance().isBugOpen(bugId)) {log.fine("Invoking workaround for "+BzChecker.getInstance().getBugState(bugId).toString()+" Bugzilla "+bugId+".  (https://bugzilla.redhat.com/show_bug.cgi?id="+bugId+")");SubscriptionManagerCLITestScript.addInvokedWorkaround(bugId);} else {invokeWorkaroundWhileBugIsOpen=false;}} catch (XmlRpcException xre) {/* ignore exception */} catch (RuntimeException re) {/* ignore exception */}
+						if (invokeWorkaroundWhileBugIsOpen) {
+							log.warning("Virtual pool '"+virtualPool.poolId+"' and and Physical pool '"+physicalPool.poolId+"' are not good candidates for this test while bug '"+bugId+"' is open.  Looking for another pair.");
+							continue;
+						}
+					}
+					// END OF WORKAROUND		
 					Assert.assertTrue(virtualProductIds.containsAll(physicalProductIds)&&physicalProductIds.containsAll(virtualProductIds), "Provided product ids from virtual pool '"+virtualPool.poolId+"' and physical pool '"+physicalPool.poolId+"' sharing a common productId '"+virtualPool.productId+"' should be the same.");
 					
 					// configure a temporary product cert directory containing only these provided product certs
