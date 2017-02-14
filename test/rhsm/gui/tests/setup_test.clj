6dfa7f70e031f6088bb6da2b44011499bc20080c
Jan Stavěl @ RedHat <jstavel@redhat.com>
2017-02-14 12:42:37 +0100

    try to use newer version of bugzilla-testng library

--- test/rhsm/gui/tests/setup_test.clj
+++ test/rhsm/gui/tests/setup_test.clj
@@ -3,7 +3,12 @@
   (:import [org.testng TestNG]))
 
 ;; this test is run this way:
-;; lein test rhsm.gui.tests.setup-test
+;;   lein test rhsm.gui.tests.setup-test
+;; or
+;;   lein test :only rhsm.gui.tests.setup-test/tier1-all-suites-test
 ;; this test is to run the whole testng machinery with our tests
-(deftest setup-consistency-test
+(deftest tier1-all-suites-test
   (TestNG/main (into-array String ["suites/sm-cli-tier1-testng-suite.xml" "suites/sm-gui-tier1-testng-suite.xml"])))
+
+(deftest tier1-all-cli-suites-test
+  (TestNG/main (into-array String ["suites/sm-cli-tier1-testng-suite.xml"])))
