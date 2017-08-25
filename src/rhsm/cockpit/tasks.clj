df5dc8521a0b5160375b33331532f2ae8d69505b
Jan Stavěl @ RedHat <jstavel@redhat.com>
2017-08-25 18:09:28 +0200

    debug statements

--- src/rhsm/cockpit/tasks.clj
+++ src/rhsm/cockpit/tasks.clj
@@ -43,6 +43,8 @@
 (defn log-user-in
   [driver username password]
   (log/info "log-user-in")
+  (println "username" username)
+  (println "password" password)
   (.. (WebDriverWait. driver 10)
       (until (ExpectedConditions/visibilityOfElementLocated
               (By/id "login-user-input")))
