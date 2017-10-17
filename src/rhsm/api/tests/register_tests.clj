197adf256e93f7cbd10a3eeb119c12c86204d7a2
Jan Stavel <jstavel@redhat.com>
2017-10-17 23:05:33 +0200

    test for dbus Register to reflect to identity change (#704)

--- src/rhsm/api/tests/register_tests.clj
+++ src/rhsm/api/tests/register_tests.clj
@@ -26,6 +26,28 @@
            [com.github.redhatqe.polarize.metadata TestDefinition]
            [com.github.redhatqe.polarize.metadata DefTypes$Project]))
 
+(defn ^{Test {:groups ["DBUS"
+                       "API"
+                       "tier1"]
+              :description "Given a dbus service rhsm is active
+When I call 'introspect' using busctl for com.redhat.RHSM1
+   and an interface /com/redhat/RHSM1/Entitlement
+Then the methods list contains of methods 'GetStatus', 'GetPools'
+"
+              :dataProvider "register-socket"}
+        TestDefinition {:projectID [`DefTypes$Project/RedHatEnterpriseLinux7]}}
+  register_methods
+  [ts socket]
+  (let [methods-of-the-object
+        (->> (format "busctl --address=unix:abstract=%s introspect com.redhat.RHSM1 /com/redhat/RHSM1/Register" socket)
+             run-command
+             :stdout
+             clojure.string/split-lines
+             (filter (fn [s] (re-find #"[\ \t]method[\ \t]" s)))
+             (map (fn [s] (clojure.string/replace s #"^([^\ \t]+).*" "$1")))
+             (into #{}))]
+    (verify (clojure.set/subset? #{".Register" ".RegisterWithActivationKeys"} methods-of-the-object))))
+
 (defn ^{Test {:groups ["registration"
                        "DBUS"
                        "API"
@@ -43,22 +65,68 @@ When I
            (a :guard #(< (Integer. %) 7 )) (throw (SkipException. "busctl is not available in RHEL6"))
            :else nil))
   (run-command "subscription-manager unregister")
-  (let [response (-> (format "busctl --address=unix:abstract=%s call com.redhat.RHSM1 /com/redhat/RHSM1/Register com.redhat.RHSM1.Register Register 'sssa{sv}' %s %s %s 0"
+  (let [response (-> (format "busctl --address=unix:abstract=%s call com.redhat.RHSM1 /com/redhat/RHSM1/Register com.redhat.RHSM1.Register Register 'sssa{sv}a{sv}' %s %s %s 0 0"
                              socket (@config :owner-key) (@config :username) (@config :password))
                      run-command)]
     (let [[parsed-response rest-string] (-> response  :stdout (.trim) dbus/parse)]
       (verify (->> rest-string (= ""))) ;; no string left unparsed
-      (verify (->> parsed-response keys (= ["content" "status" "headers"])))
-      (verify (-> parsed-response (get "status") (= 200)))
-      (let [response-headers (get parsed-response "headers")]
-        (verify (-> response-headers keys (= ["date"
-                                              "x-version"
-                                              "transfer-encoding"
-                                              "x-candlepin-request-uuid"
-                                              "content-type"
-                                              "server"]))))
-      ;;(let [content (-> parsed-response (get "content"))])
-      )))
+      (assert (= (type parsed-response) java.lang.String))
+      (let [data (-> parsed-response (str/replace #"\\\"" "\"") json/read-str)]
+        (verify (-> data keys set
+                    (clojure.set/superset? #{"entitlementStatus"
+                                             "capabilities"
+                                             "owner"
+                                             "created"
+                                             "contentTags"
+                                             "contentAccessMode"
+                                             "id"
+                                             "autoheal"
+                                             "installedProducts"
+                                             "name"
+                                             "uuid"})))))))
+
+(defn ^{Test {:groups ["registration"
+                       "DBUS"
+                       "API"
+                       "tier1"
+                       "blockedByBug-1477958"]
+              :description "Given a system is unregistered
+   and there is a simple activation key for my account
+When I
+"
+              :dataProvider "register-socket"}
+        TestDefinition {:projectID [`DefTypes$Project/RedHatEnterpriseLinux7]}}
+  dbus_register_reflects_identity_change
+  [_ socket]
+  (let [[_ major minor] (re-find #"(\d)\.(\d)" (-> :true get-release :version))]
+    (match major
+           (a :guard #(< (Integer. %) 7 )) (throw (SkipException. "busctl is not available in RHEL6"))
+           :else nil))
+  (run-command "subscription-manager unregister")
+  (-> (format "subscription-manager register --username=%s --password=%s --org=%s"
+               (@config :username) (@config :password) (@config :owner-key))
+       run-command   :stdout  (.contains "Registering to:")  verify)
+  (run-command "subscription-manager unregister")
+  (let [response (-> (format "busctl --address=unix:abstract=%s call com.redhat.RHSM1 /com/redhat/RHSM1/Register com.redhat.RHSM1.Register Register 'sssa{sv}a{sv}' %s %s %s 0 0"
+                             socket (@config :owner-key) (@config :username) (@config :password))
+                     run-command)]
+    (let [[response-data rest-string] (-> response  :stdout (.trim) dbus/parse)]
+      (verify (->> rest-string (= ""))) ;; no string left unparsed
+      (assert (= (type response-data) java.lang.String))
+      (let [data (-> response-data (str/replace #"\\\"" "\"") json/read-str)]
+        (verify (-> data keys set
+                    (clojure.set/superset? #{"entitlementStatus"
+                                             "capabilities"
+                                             "owner"
+                                             "created"
+                                             "contentTags"
+                                             "contentAccessMode"
+                                             "id"
+                                             "autoheal"
+                                             "installedProducts"
+                                             "name"
+                                             "uuid"})))
+        ))))
 
 (defn ^{DataProvider {:name "register-socket"}}
   register_socket
--- test/rhsm/api/tests/register_test.clj
+++ test/rhsm/api/tests/register_test.clj
@@ -1,6 +1,7 @@
 (ns rhsm.api.tests.register-test
   (:require  [clojure.test :refer :all]
              [rhsm.api.tests.register_tests :as tests]
+             [rhsm.gui.tasks.test-config :as c]
              [rhsm.gui.tests.base :as base]
              [clojure.data.json :as json]))
 
@@ -9,8 +10,10 @@
 ;; lein test :only rhsm.dbus.tests.activation-key-test/register-using-activation-key-test
 ;;
 
-;; ;; initialization of our testware
-;;(use-fixtures :once (fn [f] (base/startup nil) (f)))
+;; initialization of our testware
+(use-fixtures :once (fn [f]
+                      (c/init)
+                      (f)))
 
 ;; (deftest json-test
 ;;   (let [json-str (slurp "resources/content-of-register-response.json")]
@@ -26,9 +29,17 @@
 ;;     )
 ;;   )
 
-;; (deftest register-test
-;;   (let [socket (tests/register_socket nil)]
-;;     (tests/register nil (-> socket first first))))
+(deftest register-methods-test
+  (let [socket (tests/register_socket nil)]
+    (tests/register_methods nil (-> socket first first))))
+
+(deftest register-using-dbus-test
+  (let [socket (tests/register_socket nil)]
+    (tests/register_using_dbus nil (-> socket first first))))
+
+(deftest dbus_register_reflects_identity_change-test
+  (let [socket (tests/register_socket nil)]
+    (tests/dbus_register_reflects_identity_change nil (-> socket first first))))
 
 ;; (deftest attach-subscriptions-even-wrong-org-was-used-before-test
 ;;   (tests/attach_subscriptions_even_wrong_org_was_used_before nil))
