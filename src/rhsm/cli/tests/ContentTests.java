2e4ba8f6bdc2fcc50ee764bf79d67346b4ef5d73
John Sefler <jsefler@redhat.com>
2018-01-31 16:30:20 -0500

    added a WORKAROUND for bug 1526622 - "the productid plugin should never delete a /etc/pki/product-default/<ID>.pem cert provided by the redhat-release-<VARIANT>.rpm" to afterInstallAndRemoveAnyPackageFromEnabledRepoAfterSubscribingToPool()

--- src/rhsm/cli/tests/ContentTests.java
+++ src/rhsm/cli/tests/ContentTests.java
@@ -474,6 +474,19 @@ public class ContentTests extends SubscriptionManagerCLITestScript{
 				client.runCommandAndWait("rm -f "+productCertAfterTest.file);
 			}
 		}
+		
+		
+		// might also have to re-install the default-product
+		// TEMPORARY WORKAROUND
+		boolean invokeWorkaroundWhileBugIsOpen = true;
+		String bugId="1526622"; // Bug 1526622 - the productid plugin should never delete a /etc/pki/product-default/<ID>.pem cert provided by the redhat-release-<VARIANT>.rpm
+		try {if (invokeWorkaroundWhileBugIsOpen&&BzChecker.getInstance().isBugOpen(bugId)) {log.fine("Invoking workaround for "+BzChecker.getInstance().getBugState(bugId).toString()+" Bugzilla "+bugId+".  (https://bugzilla.redhat.com/show_bug.cgi?id="+bugId+")");SubscriptionManagerCLITestScript.addInvokedWorkaround(bugId);} else {invokeWorkaroundWhileBugIsOpen=false;}} catch (BugzillaAPIException be) {/* ignore exception */} catch (RuntimeException re) {/* ignore exception */} 
+		if (invokeWorkaroundWhileBugIsOpen) {
+			restoreYumBeakerRepos();
+			restoreRedHatRelease();
+			removeYumBeakerRepos();
+		}
+		// END OF WORKAROUND
 	}
 	
 	
